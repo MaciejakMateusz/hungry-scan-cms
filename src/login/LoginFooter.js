4f565d442d209735293e90cde39feb959dbf32e9
MaciejakMateusz <maciejakmateusz@gmail.com>
2024-06-02 11:05:41 +0200

    Created initial style for LoginPage and its children

--- src/login/LoginFooter.js
+++ src/login/LoginFooter.js
@@ -3,12 +3,8 @@ import React from "react";
 export const LoginFooter = () => {
     return (
         <>
-            <footer className="footer bg-gray-200">
-                <div className="container">
-                    <div className="text-center my-auto">
-                        <span>&copy;HungryScan | Powered by &copy;HackyBear 2024</span>
-                    </div>
-                </div>
+            <footer className="login-footer">
+                <span>&copy;HungryScan | Powered by &copy;HackyBear 2024</span>
             </footer>
         </>
     );
--- src/login/LoginPage.js
+++ src/login/LoginPage.js
@@ -3,6 +3,7 @@ import {LoginForm} from "./LoginForm";
 import {LoginFooter} from "./LoginFooter";
 import {getDecodedJwt} from "../utils";
 import {Navigate} from "react-router-dom";
+import {Helmet} from "react-helmet";
 
 export const LoginPage = () => {
     const [redirect, setRedirect] = useState(false);
@@ -19,24 +20,17 @@ export const LoginPage = () => {
     }
     return (
         <>
-            <div className="container">
-                <div className="row justify-content-center">
-                    <div className="col-xl-10 col-lg-12 col-md-9">
-                        <div className="card o-hidden border-0 shadow-lg my-5">
-                            <div className="card-body p-2">
-                                <div className="row">
-                                    <div className="col-lg-12">
-                                        <div className="p-5">
-                                            <LoginForm/>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+            <Helmet>
+                <title>HungryScan CMS Login</title>
+            </Helmet>
+            <div className="login-grid">
+                <div className="login-container">
+                    <div className="login-box">
+                        <LoginForm/>
                     </div>
                 </div>
+                <LoginFooter/>
             </div>
-            <LoginFooter/>
         </>
     );
 }
