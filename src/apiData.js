d390696ae2e0d98cf3c81354f13436c91042c396
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-02-15 09:23:46 +0100

    Created new env var to access REST API

--- src/apiData.js
+++ src/apiData.js
@@ -1,4 +1,4 @@
-export const apiHost = "http://localhost:8082";
+export const apiHost = process.env.HS_CORE_HOST;
 export const imagesPath = apiHost + "/images";
 export const qrPath = apiHost + "/images/qr-codes";
 export const qrName = "/QR code - HungryScan.png";
