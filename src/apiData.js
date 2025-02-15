a5b324b03024d87f2834f9af2ed5475f5ec50a70
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-02-15 10:12:52 +0100

    Updated REST API host env var name

--- src/apiData.js
+++ src/apiData.js
@@ -1,4 +1,4 @@
-export const apiHost = process.env.HS_CORE_HOST;
+export const apiHost = process.env.REACT_APP_CORE_HOST;
 export const imagesPath = apiHost + "/images";
 export const qrPath = apiHost + "/images/qr-codes";
 export const qrName = "/QR code - HungryScan.png";
