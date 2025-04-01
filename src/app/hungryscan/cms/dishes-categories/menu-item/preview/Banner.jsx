102236ac1cba2ebe86fb6f1d5de1e9f0c6635fb2
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-04-01 19:35:31 +0200

    Updated Banner to display list of chosen banners instead of single values

--- src/app/hungryscan/cms/dishes-categories/menu-item/preview/Banner.jsx
+++ src/app/hungryscan/cms/dishes-categories/menu-item/preview/Banner.jsx
@@ -2,15 +2,14 @@ import React from "react";
 import {useSelector} from "react-redux";
 
 export const Banner = () => {
-    const {fileName, banner} = useSelector(state => state.dishForm.form);
-    const bannerValue = banner?.value;
-    const bannerLabel = banner?.label;
+    const {fileName} = useSelector(state => state.dishForm.form);
+    const {chosenBanners} = useSelector(state => state.dishForm.fetchBanners);
 
-    if (bannerValue === 'isBestseller') {
-        return (<span className={`details-banner ${!fileName ? 'no-image' : ''}`}>{bannerLabel}</span>);
-    } else if (bannerValue === 'isNew') {
-        return (<span className={`details-banner ${!fileName ? 'no-image' : ''}`}>{bannerLabel}</span>);
+    if (chosenBanners?.length === 0) {
+        return (<></>);
     }
 
-    return (<></>);
+    return (chosenBanners.map(banner => (
+        <span className={`details-banner ${!fileName ? 'no-image' : ''}`}>{banner.label}</span>
+    )));
 }
