30f6ffb5b310701c8f1131ca556b445a720e7161
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-04-04 21:15:28 +0200

    Created ReorderCategoriesDialog with dnd context

---
+++ src/app/hungryscan/cms/dialog-windows/ReorderCategoriesDialog.jsx
@@ -0,0 +1,76 @@
+import {useTranslation} from "react-i18next";
+import React, {useEffect, useState} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {
+    setReorderCategoriesDialogActive,
+    updateCategoriesOrder
+} from "../../../../slices/dishesCategoriesSlice";
+import {DndContext} from '@dnd-kit/core';
+import {arrayMove, SortableContext} from '@dnd-kit/sortable';
+
+import {ReorderCategoryPosition} from "../dishes-categories/category/ReorderCategoryPosition";
+
+export const ReorderCategoriesDialog = () => {
+    const {t} = useTranslation();
+    const dispatch = useDispatch();
+    const {menu} = useSelector(state => state.cms.fetchActiveMenu);
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        if (menu?.categories) {
+            setCategories(menu.categories);
+        }
+    }, [menu]);
+
+    const discardDialog = () => {
+        dispatch(setReorderCategoriesDialogActive(false));
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        dispatch(updateCategoriesOrder({categories: categories}));
+        dispatch(setReorderCategoriesDialogActive(false));
+    }
+
+    const handleDragEnd = async event => {
+        const {active, over} = event;
+        if (!over || active.id === over.id) return;
+
+        const oldIndex = categories.findIndex(c => c.id.toString() === active.id);
+        const newIndex = categories.findIndex(c => c.id.toString() === over.id);
+
+        const categoriesOrdered = arrayMove(categories, oldIndex, newIndex);
+        const newCategories = categoriesOrdered.map((c, index) => ({
+            ...c,
+            displayOrder: index + 1,
+        }));
+
+        setCategories(newCategories);
+    };
+
+    return (
+        <>
+            <div className={'overlay'}></div>
+            <div className={'reordering-dialog'}>
+                <div className={'reordering-dialog-content'}>
+                    <p>Reorder categories</p>
+                    <DndContext onDragEnd={(e) => handleDragEnd(e)}>
+                        <SortableContext items={categories.map(c => c.id.toString())}>
+                            {categories.map(category => (
+                                <ReorderCategoryPosition key={`${category.id}-${category.displayOrder}`}
+                                                         id={category.id.toString()}
+                                                         category={category}/>
+                            ))}
+                        </SortableContext>
+                    </DndContext>
+                </div>
+                <div className={'reordering-dialog-footer'}>
+                <button onClick={discardDialog} className={'general-button cancel'}>{t('cancel')}</button>
+                    <form onSubmit={handleSubmit} style={{all: 'unset'}}>
+                        <button type="submit" className={'general-button'}>{t('confirm')}</button>
+                    </form>
+                </div>
+            </div>
+        </>
+    );
+}
