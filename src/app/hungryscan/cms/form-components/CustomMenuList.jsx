7ec341ebde4a8f4230a1b559d178f1925b5fd1a9
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-04-27 15:31:11 +0200

    Updated CustomMenuList, used selectRef to hide the menu after button is clicked

--- src/app/hungryscan/cms/form-components/CustomMenuList.jsx
+++ src/app/hungryscan/cms/form-components/CustomMenuList.jsx
@@ -4,32 +4,24 @@ import {components} from 'react-select';
 export const CustomMenuList = (props) => {
     const {
         children,
-        selectProps: {onAdd, buttonText}
+        selectProps: {onAdd, buttonText, selectRef}
     } = props;
 
+    const handleClick = () => {
+        onAdd();
+        if (selectRef && selectRef.current) {
+            selectRef.current.blur();
+        }
+    };
+
     return (
-        <div style={{
-            display: 'flex',
-            flexDirection: 'column',
-            height: '100%',
-        }}>
-            <div style={{flexGrow: 1, overflowY: 'auto',}}>
+        <div className={'custom-menu-list-container'}>
+            <div className={'custom-menu-list-children'}>
                 <components.MenuList {...props}>
                     {children}
                 </components.MenuList>
             </div>
-            <div style={{
-                padding: '10px',
-                textAlign: 'center',
-                borderTop: '1px solid #eee',
-                cursor: 'pointer',
-                color: '#fff',
-                backgroundColor: '#9746FF',
-                fontWeight: 500,
-                borderBottomLeftRadius: '20px',
-                borderBottomRightRadius: '20px',
-            }}
-                 onClick={onAdd}>
+            <div className={'custom-menu-list-button'} onClick={handleClick}>
                 + {buttonText}
             </div>
         </div>
