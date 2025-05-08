7cc7af84d2ff16dd9404f4eb6d8e0177de4dbad2
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-05-08 17:00:39 +0200

    Removed imagesPath and replaced it with s3BucketUrl

--- src/apiData.js
+++ src/apiData.js
@@ -1,4 +1,4 @@
 export const apiHost = process.env.REACT_APP_CORE_HOST;
-export const imagesPath = apiHost + "/images";
 export const qrPath = apiHost + "/images/qr-codes";
 export const qrName = "/QR code - HungryScan.png";
+export const s3BucketUrl = process.env.REACT_APP_S3_BUCKET;
