1a87a8fda54c0d811600d3b1e2b5c96b28c7abcb
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-07-16 06:23:08 +0200

    Updated useDayHours to be in 30 minute intervals

--- src/hooks/useDayHours.js
+++ src/hooks/useDayHours.js
@@ -1,10 +1,18 @@
-import {useMemo} from "react";
+import { useMemo } from "react";
 
 export const useDayHours = () => {
-    return useMemo(() =>
-            Array.from({length: 24}, (_, hour) => ({
+    return useMemo(() => {
+        const times = [];
+        for (let hour = 0; hour < 24; hour++) {
+            times.push({
                 value: hour.toString().padStart(2, '0') + ':00:00',
                 label: hour.toString().padStart(2, '0') + ':00'
-            }))
-        , []);
+            });
+            times.push({
+                value: hour.toString().padStart(2, '0') + ':30:00',
+                label: hour.toString().padStart(2, '0') + ':30'
+            });
+        }
+        return times;
+    }, []);
 };
