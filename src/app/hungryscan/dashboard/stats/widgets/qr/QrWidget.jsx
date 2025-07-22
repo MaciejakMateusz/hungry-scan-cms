57c709eeb70f85cb23fcaed4c6fb0d1e38bc0aec
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-07-22 15:31:53 +0200

    Updated QrWidget - fetching from S3 bucket

--- src/app/hungryscan/dashboard/stats/widgets/qr/QrWidget.jsx
+++ src/app/hungryscan/dashboard/stats/widgets/qr/QrWidget.jsx
@@ -1,20 +1,28 @@
 import React from "react";
-import {qrName, qrPath} from "../../../../../../apiData";
+import {s3BucketUrl} from "../../../../../../apiData";
 import {useTranslation} from "react-i18next";
 import {DownloadIcon} from "../../../../../icons/DownloadIcon";
 import {TrendingUpIcon} from "../../../../../icons/TrendingUpIcon";
 import {useSelector} from "react-redux";
+import {Img} from "react-image";
+import {PlaceholderImgIcon} from "../../../../../icons/PlaceholderImgIcon";
 
 export const QrWidget = () => {
     const {t} = useTranslation();
     const {data} = useSelector(state => state.statistics.scanStats);
+    const {restaurant} = useSelector(state => state.dashboard.view);
+    const restaurantData = restaurant?.value;
 
     return (
         <div className={'statistic-widget qr-code'}>
             <div className={'widget-container qr-code'}>
                 <div className={'qr-code-download-area'}>
                     <div className={'qr-img-container'}>
-                        <img className={'qr-img'} src={qrPath + qrName} alt={'qr-code'}/>
+                        <Img className={'qr-img'}
+                             alt={'qr-code'}
+                             src={`${s3BucketUrl}/qr/basic/${restaurantData?.id}.png?t=${restaurantData?.updated}`}
+                             unloader={<PlaceholderImgIcon/>}
+                        />
                     </div>
                     <div className={'qr-img-download-btn'}>{t('download')}<DownloadIcon/></div>
                 </div>
