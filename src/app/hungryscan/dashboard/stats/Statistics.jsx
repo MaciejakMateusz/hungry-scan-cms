cc65467ff4d1c88e1e8607187456dc498f94667b
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-09-08 18:55:21 +0200

    Updated Statistics component

--- src/app/hungryscan/dashboard/stats/Statistics.jsx
+++ src/app/hungryscan/dashboard/stats/Statistics.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import {Widgets} from "./widgets/Widgets";
 import {PeriodicSummary} from "./PeriodicSummary";
+import {Helmet} from "react-helmet";
+import {useTranslation} from "react-i18next";
 
 export const Statistics = () => {
+    const {t} = useTranslation();
+    
     return (
         <div className={'statistics-content'}>
+            <Helmet>
+                <title>{t('dashboard')} - {t('statistics')}</title>
+            </Helmet>
             <PeriodicSummary/>
             <Widgets/>
         </div>
