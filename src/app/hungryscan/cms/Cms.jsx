5be4a2f54692a40e58e87bc6d0a44fef20f5c659
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-09-17 18:57:39 +0200

    Updated Cms component

--- src/app/hungryscan/cms/Cms.jsx
+++ src/app/hungryscan/cms/Cms.jsx
@@ -64,7 +64,7 @@ export const Cms = () => {
     const switchView = (viewName) => {
         if (isInEditMode) {
             setSwitchViewDialog(viewName)
-        } else {
+        } else if (viewName !== currentView) {
             clearCmsState();
             dispatch(setCurrentView(viewName));
         }
