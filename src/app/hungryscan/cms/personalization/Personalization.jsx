73b6288ee640f7fe38c1441557297ebec87544f9
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-09-22 16:20:55 +0200

    Updated Personalization

--- src/app/hungryscan/cms/personalization/Personalization.jsx
+++ src/app/hungryscan/cms/personalization/Personalization.jsx
@@ -1,9 +1,29 @@
 import React from "react";
 import {useTranslation} from "react-i18next";
 import {MenuMobilePreview} from "./MenuMobilePreview";
+import {PersonalizationForm} from "./form/PersonalizationForm";
+import {useDispatch, useSelector} from "react-redux";
+import {postPersonalization, setPersonalizationUpdated} from "../../../../slices/personalizationSlice";
+import {useConfirmationMessage} from "../../../../hooks/useConfirmationMessage";
+import {useFetchCurrentRestaurant} from "../../../../hooks/useFetchCurrentRestaurant";
+import {fetchActiveMenu} from "../../../../slices/cmsSlice";
 
 export const Personalization = () => {
     const {t} = useTranslation();
+    const dispatch = useDispatch();
+    const renderConfirmation = useConfirmationMessage(setPersonalizationUpdated);
+    const {activeMenu} = useSelector(state => state.globalParams.globalParams);
+    const fetchCurrentRestaurant = useFetchCurrentRestaurant()
+
+    const handleFormSubmit = async (e) => {
+        e.preventDefault();
+        const resultAction = await dispatch(postPersonalization({activeMenu}));
+        if (postPersonalization.fulfilled.match(resultAction)) {
+            await fetchCurrentRestaurant();
+            await dispatch(fetchActiveMenu());
+            renderConfirmation();
+        }
+    }
 
     return (
         <div className={'background'}>
@@ -13,12 +33,13 @@ export const Personalization = () => {
                         <div className={'form-header'}>
                             {t('personalization')}
                         </div>
-                        <form className={'menu-item-form'}>
-                            <div className={'form-footer'}>
-                                <div className={'general-button cancel'}>
-                                    {t('cancel')}
+                        <form className={'padded-form-container'}>
+                            <PersonalizationForm/>
+                            <div className={'personalization-form-footer'}>
+                                <div className={'general-button submit'}
+                                     onClick={handleFormSubmit}>
+                                    {t('save')}
                                 </div>
-                                <div className={'general-button submit'}>{t('save')}</div>
                             </div>
                         </form>
                     </div>
