18153b439b6c120ceddf1c820e36037f5b2ebbad
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-09-23 17:57:31 +0200

    Created useAdditionContextPositions hook

---
+++ src/hooks/useAdditionContextPositions.js
@@ -0,0 +1,32 @@
+import {useTranslation} from "react-i18next";
+import {EditIcon} from "../app/icons/EditIcon";
+import {DeleteIcon} from "../app/icons/DeleteIcon";
+import {useDispatch} from "react-redux";
+import {setAddition, setAdditionDialogActive, setAdditionToRemove, setIsNewAddition} from "../slices/additionsSlice";
+
+export const useAdditionContextPositions = ({ingredient, setContextWindowActive}) => {
+    const {t} = useTranslation();
+    const dispatch = useDispatch();
+
+    return [
+        {
+            id: 'edit',
+            name: t('edit'),
+            icon: <EditIcon width={'25'} height={'25'}/>,
+            handler: () => {
+                dispatch(setAddition(ingredient));
+                dispatch(setIsNewAddition(false));
+                dispatch(setAdditionDialogActive(true));
+                setContextWindowActive(false)
+            }
+        },
+        {
+            id: 'remove',
+            name: t('remove'),
+            icon: <DeleteIcon width={'25'} height={'25'}/>,
+            handler: () => {
+                dispatch(setAdditionToRemove(ingredient));
+                setContextWindowActive(false)
+            }
+        }];
+}
