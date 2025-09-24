4dab9f5e44fb8399b5ca70d942193adfeca35cd0
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-09-24 20:38:14 +0200

    Created LetterGroup component

---
+++ src/app/hungryscan/cms/additions/LetterGroup.jsx
@@ -0,0 +1,23 @@
+import React, {useEffect} from "react";
+import {AdditionRecord} from "../shared-components/AdditionRecord";
+
+export const LetterGroup = ({letter, ingredients}) => {
+
+    return (
+        <div className={'letter-group-container'}>
+            <div className={'letter-group-header'}>
+                {letter}
+            </div>
+            <div className={'letter-group-content'}>
+                {ingredients?.map(ingredient => (
+                    <AdditionRecord key={ingredient.id}
+                                    ingredient={ingredient}
+                                    name={ingredient.name}
+                                    price={ingredient.price}
+                                    available={ingredient.available}
+                    />))
+                }
+            </div>
+        </div>
+    );
+}
