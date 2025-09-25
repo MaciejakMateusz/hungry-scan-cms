bfdcd0a6ce9a6427302a95feae9823d2af7e5b1e
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-09-25 21:46:42 +0200

    Created RecordOptionsButton component

---
+++ src/app/hungryscan/cms/shared-components/RecordOptionsButton.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import {ThreeDotsIcon} from "../../../icons/ThreeDotsIcon";
+import {ContextMenu} from "./ContextMenu";
+
+export const RecordOptionsButton = ({
+                                        onClick,
+                                        contextWindowActive,
+                                        contextPositions,
+                                        obj,
+                                        detailsActive,
+                                        contextRef,
+                                        windowPosition,
+                                        className,
+                                        dotsFill
+                                    }) => {
+    return (
+        <div className={'relative-container'}>
+            <div className={className}
+                 tabIndex={-1}
+                 onClick={onClick}>
+                <ThreeDotsIcon fill={dotsFill}/>
+            </div>
+            {contextWindowActive &&
+                <ContextMenu positions={contextPositions}
+                             obj={obj}
+                             detailsActive={detailsActive}
+                             contextRef={contextRef}
+                             position={windowPosition}/>}
+        </div>
+    );
+}
