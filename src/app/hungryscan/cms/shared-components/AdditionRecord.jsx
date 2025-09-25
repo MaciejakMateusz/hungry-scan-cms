dcc7782d70677502e1a5bb979d0dcf8080cc889d
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-09-25 21:47:00 +0200

    Updated AdditionRecord component

--- src/app/hungryscan/cms/shared-components/AdditionRecord.jsx
+++ src/app/hungryscan/cms/shared-components/AdditionRecord.jsx
@@ -1,9 +1,8 @@
 import React, {useRef, useState} from "react";
 import {getTranslation} from "../../../../locales/langUtils";
-import {ThreeDotsIcon} from "../../../icons/ThreeDotsIcon";
-import {ContextMenu} from "./ContextMenu";
 import {useAdditionContextPositions} from "../../../../hooks/useAdditionContextPositions";
 import {useOutsideClick} from "../../../../hooks/useOutsideClick";
+import {RecordOptionsButton} from "./RecordOptionsButton";
 
 export const AdditionRecord = ({ingredient, name, price}) => {
     const contextRef = useRef();
@@ -22,19 +21,14 @@ export const AdditionRecord = ({ingredient, name, price}) => {
                     <span>{getTranslation(name)}</span>
                     <div className={'addition-price-actions-group'}>
                         <span>+ {price.toFixed(2)} zł</span>
-                        <div className={'relative-container'}>
-                            <div className={'record-context-actions-button'}
-                                 tabIndex={-1}
-                                 onClick={() => setContextWindowActive(!contextWindowActive)}>
-                                <ThreeDotsIcon/>
-                            </div>
-                            {contextWindowActive &&
-                                <ContextMenu positions={additionContextPositions}
+                        <RecordOptionsButton className={'record-context-actions-button'}
+                                             onClick={() => setContextWindowActive(!contextWindowActive)}
+                                             contextWindowActive={contextWindowActive}
+                                             contextPositions={additionContextPositions}
                                              obj={ingredient}
                                              detailsActive={false}
                                              contextRef={contextRef}
-                                             position={{left: '-150px', top: '30px'}}/>}
-                        </div>
+                                             windowPosition={{left: '-150px', top: '30px'}}/>
                     </div>
                 </div>
             </div>
