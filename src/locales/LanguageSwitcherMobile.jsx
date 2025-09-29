2f0c595074bb920ad3cd63a86130ad68270cfca9
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-09-29 20:35:54 +0200

    Created new translation files

---
+++ src/locales/LanguageSwitcherMobile.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import {ReactSVG} from "react-svg";
+import {useState} from "react";
+import {useTranslation} from "react-i18next";
+import {getLanguage} from "./langUtils";
+import {supportedLanguages} from "../i18n";
+
+export const LanguageSwitcherMobile = () => {
+    const {i18n} = useTranslation();
+    const activeLng = getLanguage() || 'pl-PL';
+    const supported = Object.keys(supportedLanguages).map(languageKey => languageKey.toUpperCase());
+    const [isDropped, setIsDropped] = useState(false);
+
+    const formatToUppercase = language => {
+        return language === 'pl-PL' ? 'PL' : language.toUpperCase();
+    }
+
+    const changeLanguageMobile = lng => {
+        i18n.changeLanguage(lng.toLowerCase());
+        document.cookie = `i18next=${lng.toLowerCase()}; path=/`;
+        setIsDropped(false);
+    }
+
+    return (
+        <div className={'lang-switcher-mobile-container non-selectable'}>
+            <div className={'lang-switcher-mobile'} onClick={() => setIsDropped(!isDropped)}>
+                <span className={'lang-switcher-mobile-wrapper'}>
+                    <span className={'lng-mobile-display'}>{formatToUppercase(activeLng)}</span>
+                    <ReactSVG src={`${process.env.PUBLIC_URL}/theme/icons/drop-icon.svg`}
+                              className={`drop-icon ${isDropped && 'dropped'}`}/>
+                </span>
+                {isDropped && supported.filter(l => l !== formatToUppercase(activeLng)).map(l => (
+                    <div className={'lng-mobile'}
+                         onClick={(e) => {
+                             e.stopPropagation();
+                             changeLanguageMobile(l);
+                         }}
+                         key={l}>
+                        {l}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
