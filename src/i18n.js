dc6c75e45d0b7f7358be952ef79fb6c4c5d346af
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-09-29 20:35:36 +0200

    Updated i18n.js

--- src/i18n.js
+++ src/i18n.js
@@ -1,31 +1,39 @@
 import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
+import {initReactI18next} from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
 import translation_en from './locales/en/translation_en.json';
 import translation_pl from './locales/pl/translation_pl.json';
+import translation_de from './locales/de/translation_de.json';
+import translation_fr from './locales/fr/translation_fr.json';
+import translation_es from './locales/es/translation_es.json';
+import translation_uk from './locales/uk/translation_uk.json';
 
-// Configure the language detector
 const languageDetector = new LanguageDetector(null, {
     order: ['cookie', 'localStorage', 'navigator', 'htmlTag', 'path', 'subdomain'],
     caches: ['cookie']
 });
 
+export const supportedLanguages = {
+    pl: {translation: translation_pl},
+    en: {translation: translation_en},
+    de: {translation: translation_de},
+    fr: {translation: translation_fr},
+    es: {translation: translation_es},
+    uk: {translation: translation_uk}
+};
+
 i18n
     .use(languageDetector)
     .use(initReactI18next)
     .init({
-        resources: {
-            en: { translation: translation_en },
-            pl: { translation: translation_pl },
-        },
-        lng: 'pl', // Default language
-        fallbackLng: 'pl',
+        resources: supportedLanguages,
+        lng: process.env.REACT_APP_DEFAULT_LANGUAGE,
+        fallbackLng: process.env.REACT_APP_DEFAULT_LANGUAGE,
         interpolation: {
             escapeValue: false,
         },
         detection: {
-            // Options for language detector
             order: ['cookie', 'localStorage', 'navigator'],
             caches: ['cookie']
         }
