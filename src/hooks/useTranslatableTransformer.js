a1d5c7d6cbbeea6486ceab08975055e04f65015d
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-09-30 19:15:59 +0200

    Created useTranslatableTransformer hook

---
+++ src/hooks/useTranslatableTransformer.js
@@ -0,0 +1,10 @@
+import {createTranslatable, updateTranslatable} from "../locales/langUtils";
+
+export const useTranslatableTransformer = ({obj, key}) => {
+    return value => {
+        if (!obj[key]) {
+            return createTranslatable(value);
+        }
+        return updateTranslatable(obj[key], value)
+    }
+}
