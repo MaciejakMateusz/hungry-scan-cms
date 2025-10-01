d3ac53de9692c8a6fa52229326be80df6c132b2c
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-10-01 21:48:40 +0200

    Created Variants

---
+++ src/app/hungryscan/cms/dishes-categories/menu-item/variants/Variants.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {arrayMove, SortableContext} from "@dnd-kit/sortable";
+import {DndContext} from "@dnd-kit/core";
+import {setVariants} from "../../../../../../slices/dishFormSlice";
+import {VariantPosition} from "./VariantPosition";
+import {setIsNewVariant, setVariantDialogActive} from "../../../../../../slices/variantsSlice";
+import {useTranslation} from "react-i18next";
+import {VariantFormDialog} from "./VariantFormDialog";
+
+export const Variants = () => {
+    const dispatch = useDispatch();
+    const {t} = useTranslation();
+    const {variants} = useSelector(state => state.dishForm.form);
+    const {variantDialogActive} = useSelector(state => state.variants.view);
+    const variantIds = variants?.map(variant => variant.id.toString());
+
+    if (!variants) {
+        return null;
+    }
+
+    const handleDragEnd = async event => {
+        const {active, over} = event;
+        if (!over || active.id === over.id) return;
+
+        const oldIndex = variants.findIndex(c => c.id.toString() === active.id);
+        const newIndex = variants.findIndex(c => c.id.toString() === over.id);
+
+        const variantsOrdered = arrayMove(variants, oldIndex, newIndex);
+        const newVariants = variantsOrdered.map((c, index) => ({
+            ...c,
+            displayOrder: index + 1,
+        }));
+
+        dispatch(setVariants(newVariants));
+    };
+
+    return (
+        <>
+            {variantDialogActive && <VariantFormDialog variants={variants}/>}
+            <div className={'menu-item-form-variants-container'}>
+                <div className={'flex-end-container'}>
+                    <div className={'new-position-button'} onClick={() => {
+                        dispatch(setIsNewVariant(true));
+                        dispatch(setVariantDialogActive(true))
+                    }}>
+                        + &nbsp;{t('newVariant')}
+                    </div>
+                </div>
+                {variants?.length === 0 && <p className={'flex-centered'}>{t('noVariantsInDish')}</p>}
+                <DndContext onDragEnd={(event) => handleDragEnd(event)}>
+                    <SortableContext items={variantIds}>
+                        {variants.map(variant => (
+                            <VariantPosition key={`${variant.id}-${variant.displayOrder}`}
+                                             id={variant.id.toString()}
+                                             variant={variant}/>
+                        ))}
+                    </SortableContext>
+                </DndContext>
+            </div>
+        </>
+    );
+
+}
