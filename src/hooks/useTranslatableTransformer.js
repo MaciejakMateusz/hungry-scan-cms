45659c46adc11fc6d5f66da72c7dd52948b3a5a5
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-10-01 21:50:58 +0200

    Updated useTranslatableTransformer hook

--- src/hooks/useTranslatableTransformer.js
+++ src/hooks/useTranslatableTransformer.js
@@ -2,7 +2,7 @@ import {createTranslatable, updateTranslatable} from "../locales/langUtils";
 
 export const useTranslatableTransformer = ({obj, key}) => {
     return value => {
-        if (!obj[key]) {
+        if (!obj?.[key]) {
             return createTranslatable(value);
         }
         return updateTranslatable(obj[key], value)
