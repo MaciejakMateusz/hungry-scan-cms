df1e052794ba47fcd15adb983f00de3a12b48c32
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-10-04 11:31:00 +0200

    Updated ThreeDotsIcon svg component

--- src/app/icons/ThreeDotsIcon.jsx
+++ src/app/icons/ThreeDotsIcon.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-export const ThreeDotsIcon = () => {
+export const ThreeDotsIcon = ({fill}) => {
     return (
         <svg width="16" height="4" viewBox="0 0 18 4" fill="none" xmlns="http://www.w3.org/2000/svg">
             <g id="Group 1022">
-                <circle id="Ellipse 288" cx="1.8" cy="1.99995" r="1.8" fill="#9746FF"/>
-                <circle id="Ellipse 289" cx="9.0002" cy="1.99995" r="1.8" fill="#9746FF"/>
-                <circle id="Ellipse 290" cx="16.2004" cy="1.99995" r="1.8" fill="#9746FF"/>
+                <circle id="Ellipse 288" cx="1.8" cy="1.99995" r="1.8" fill={fill ? fill : "#9746FF"}/>
+                <circle id="Ellipse 289" cx="9.0002" cy="1.99995" r="1.8" fill={fill ? fill : "#9746FF"}/>
+                <circle id="Ellipse 290" cx="16.2004" cy="1.99995" r="1.8" fill={fill ? fill : "#9746FF"}/>
             </g>
         </svg>
     );
