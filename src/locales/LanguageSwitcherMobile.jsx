fbd715629f9aa24c016a3a141cfa4b2de3d4eb57
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-10-05 10:44:25 +0200

    Updated LanguageSwitcherMobile component

--- src/locales/LanguageSwitcherMobile.jsx
+++ src/locales/LanguageSwitcherMobile.jsx
@@ -1,15 +1,19 @@
-import React from "react";
+import React, {useRef} from "react";
 import {ReactSVG} from "react-svg";
 import {useState} from "react";
 import {useTranslation} from "react-i18next";
 import {getLanguage} from "./langUtils";
 import {supportedLanguages} from "../i18n";
+import {useOutsideClick} from "../hooks/useOutsideClick";
 
 export const LanguageSwitcherMobile = () => {
     const {i18n} = useTranslation();
     const activeLng = getLanguage() || 'pl-PL';
     const supported = Object.keys(supportedLanguages).map(languageKey => languageKey.toUpperCase());
     const [isDropped, setIsDropped] = useState(false);
+    const dropDownRef = useRef(null);
+
+    useOutsideClick(dropDownRef, () => setIsDropped(false), isDropped);
 
     const formatToUppercase = language => {
         return language === 'pl-PL' ? 'PL' : language.toUpperCase();
@@ -21,14 +25,11 @@ export const LanguageSwitcherMobile = () => {
         setIsDropped(false);
     }
 
-    return (
-        <div className={'lang-switcher-mobile-container non-selectable'}>
-            <div className={'lang-switcher-mobile'} onClick={() => setIsDropped(!isDropped)}>
-                <span className={'lang-switcher-mobile-wrapper'}>
-                    <span className={'lng-mobile-display'}>{formatToUppercase(activeLng)}</span>
-                    <ReactSVG src={`${process.env.PUBLIC_URL}/theme/icons/drop-icon.svg`}
-                              className={`drop-icon ${isDropped && 'dropped'}`}/>
-                </span>
+    const renderDropdown = () => {
+        if (!isDropped) return;
+
+        return (
+            <div className={'lang-switcher-dropdown-container'} ref={dropDownRef}>
                 {isDropped && supported.filter(l => l !== formatToUppercase(activeLng)).map(l => (
                     <div className={'lng-mobile'}
                          onClick={(e) => {
@@ -39,6 +40,19 @@ export const LanguageSwitcherMobile = () => {
                         {l}
                     </div>
                 ))}
+            </div>
+        );
+    }
+
+    return (
+        <div className={'lang-switcher-mobile-container non-selectable'}>
+            <div className={'lang-switcher-mobile'} onClick={() => setIsDropped(!isDropped)}>
+                <span className={'lang-switcher-mobile-wrapper'}>
+                    <span className={'lng-mobile-display'}>{formatToUppercase(activeLng)}</span>
+                    <ReactSVG src={`${process.env.PUBLIC_URL}/theme/icons/drop-icon.svg`}
+                              className={`drop-icon ${isDropped && 'dropped-icon'}`}/>
+                </span>
+                {renderDropdown()}
             </div>
         </div>
     );
