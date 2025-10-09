e61bc5add35e351ae84685d98f393b78c06e2f50
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-10-09 08:11:50 +0200

    Updated RestaurantFormTemplate

--- src/app/hungryscan/dashboard/restaurant/RestaurantFormTemplate.jsx
+++ src/app/hungryscan/dashboard/restaurant/RestaurantFormTemplate.jsx
@@ -4,9 +4,11 @@ import {setAddress, setCity, setName, setPostalCode} from "../../../../slices/re
 import {NameField} from "../../cms/form-components/NameField";
 import {CustomTextField} from "../../cms/form-components/CustomTextField";
 import {OperatingHoursFieldsSet} from "./OperatingHoursFieldsSet";
+import {useTranslation} from "react-i18next";
 
 export const RestaurantFormTemplate = () => {
     const dispatch = useDispatch();
+    const {t} = useTranslation();
     const {
         name,
         address,
@@ -17,40 +19,42 @@ export const RestaurantFormTemplate = () => {
 
     return (
         <>
-            <NameField id={'restaurant-name'}
-                       value={name}
-                       onChange={(e) => dispatch(setName(e))}
-                       error={errorData}
-            />
-            <CustomTextField id={'restaurant-address'}
-                             errorField={errorData?.address}
-                             value={address}
-                             onChange={(e) => dispatch(setAddress(e))}
-                             required={true}
-                             placeholder={'Wpisz adres'}
-                             label={'Adres'}
-            />
-            <CustomTextField id={'restaurant-postalCode'}
-                             value={postalCode}
-                             onChange={(e) => dispatch(setPostalCode(e))}
-                             errorField={errorData?.postalCode}
-                             required={true}
-                             placeholder={'Wpisz kod pocztowy'}
-                             label={'Kod pocztowy'}
-            />
-            <CustomTextField id={'restaurant-city'}
-                             value={city}
-                             onChange={(e) => dispatch(setCity(e))}
-                             errorField={errorData?.city}
-                             required={true}
-                             placeholder={'Wpisz miasto'}
-                             label={'Miasto'}
-            />
             <div>
-                <OperatingHoursFieldsSet/>
-                {errorData?.settings && <span className={'form-validation-msg'}>{errorData?.settings}</span>}
+                <div className={'form-group-header'}>
+                    {t('restaurantData')}
+                </div>
+                <NameField id={'restaurant-name'}
+                           value={name}
+                           onChange={(e) => dispatch(setName(e))}
+                           error={errorData}
+                />
+                <CustomTextField id={'restaurant-address'}
+                                 errorField={errorData?.address}
+                                 value={address}
+                                 onChange={(e) => dispatch(setAddress(e))}
+                                 required={true}
+                                 placeholder={'Wpisz adres'}
+                                 label={'Adres'}
+                />
+                <CustomTextField id={'restaurant-postalCode'}
+                                 value={postalCode}
+                                 onChange={(e) => dispatch(setPostalCode(e))}
+                                 errorField={errorData?.postalCode}
+                                 required={true}
+                                 placeholder={'Wpisz kod pocztowy'}
+                                 label={'Kod pocztowy'}
+                />
+                <CustomTextField id={'restaurant-city'}
+                                 value={city}
+                                 onChange={(e) => dispatch(setCity(e))}
+                                 errorField={errorData?.city}
+                                 required={true}
+                                 placeholder={'Wpisz miasto'}
+                                 label={'Miasto'}
+                />
             </div>
-
+            <OperatingHoursFieldsSet/>
+            {errorData?.settings && <span className={'form-validation-msg'}>{errorData?.settings}</span>}
         </>
     );
 }
