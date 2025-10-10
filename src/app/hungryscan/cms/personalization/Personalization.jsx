bae0a9e22ecae0ae185c4150aa8e19a71cc6c3cb
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-10-10 17:39:47 +0200

    Updated Personalization.jsx

--- src/app/hungryscan/cms/personalization/Personalization.jsx
+++ src/app/hungryscan/cms/personalization/Personalization.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import {useTranslation} from "react-i18next";
-import {MenuMobilePreview} from "./MenuMobilePreview";
 import {PersonalizationForm} from "./form/PersonalizationForm";
 import {useDispatch, useSelector} from "react-redux";
 import {postPersonalization, setPersonalizationUpdated} from "../../../../slices/personalizationSlice";
 import {useConfirmationMessage} from "../../../../hooks/useConfirmationMessage";
 import {useFetchCurrentRestaurant} from "../../../../hooks/useFetchCurrentRestaurant";
 import {fetchActiveMenu} from "../../../../slices/cmsSlice";
+import {FormHeader} from "../shared-components/FormHeader";
 
 export const Personalization = () => {
     const {t} = useTranslation();
     const dispatch = useDispatch();
     const renderConfirmation = useConfirmationMessage(setPersonalizationUpdated);
     const {activeMenu} = useSelector(state => state.globalParams.globalParams);
+    const {isLoading} = useSelector(state => state.personalization.postPersonalization);
     const fetchCurrentRestaurant = useFetchCurrentRestaurant()
 
     const handleFormSubmit = async (e) => {
@@ -28,18 +29,10 @@ export const Personalization = () => {
     return (
         <div className={'background'}>
             <form className={'cms-padded-view-container'}>
-                <div className={'functions-header'}>
-                    <div className={'section-heading'}>
-                        {t('personalization')}
-                    </div>
-                    <div className={'flex-wrapper-gapped'}>
-                        <div className={'form-footer'}>
-                            <div className={'general-button'} onClick={handleFormSubmit}>
-                                {t('save')}
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <FormHeader formHeader={t('personalization')}
+                            onFormSubmit={handleFormSubmit}
+                            isLoading={isLoading}
+                />
                 <div className={'form-grid'}>
                     <div className={'padded-form-fragment'}>
                         <PersonalizationForm/>
