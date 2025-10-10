2fb1314af91e2a6ff67952b11608f9f8350c57d9
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-10-10 17:39:14 +0200

    Updated OperatingHoursFieldsSet.jsx

--- src/app/hungryscan/dashboard/restaurant/OperatingHoursFieldsSet.jsx
+++ src/app/hungryscan/dashboard/restaurant/OperatingHoursFieldsSet.jsx
@@ -1,4 +1,3 @@
-import {TimeField} from "../../cms/form-components/TimeField";
 import {
     setFridayAvailable,
     setFridayClosingTime,
@@ -24,129 +23,57 @@ import {
 } from "../../../../slices/restaurantSlice";
 import React from "react";
 import {useTranslation} from "react-i18next";
-import {useSelector} from "react-redux";
-import {LogicalToggle} from "../../cms/form-components/LogicalToggle";
+import {useDispatch, useSelector} from "react-redux";
+import {DayOpeningHoursRow} from "./DayOpeningHoursRow";
+import {Container} from "./OperatingHoursFieldsSet.style";
 
 export const OperatingHoursFieldsSet = () => {
     const {t} = useTranslation();
+    const dispatch = useDispatch();
     const operatingHours = useSelector(state => state.restaurant.form.settings.operatingHours);
 
     return (
         <>
-            <div>Godziny otwarcia:</div>
-            <div className={'flex-centered'}>
-                <LogicalToggle id={'mondayAvailable'}
-                               changeHandler={setMondayAvailable}
-                               value={operatingHours?.MONDAY?.available}/>
-                <div>{t('monday')}</div>
-                <TimeField name={'mondayOpeningTime'}
-                           value={operatingHours?.MONDAY?.startTime}
-                           onChange={setMondayOpeningTime}
-                           isDisabled={!operatingHours?.MONDAY?.available}
-                />
-                <TimeField name={'mondayClosingTime'}
-                           value={operatingHours?.MONDAY?.endTime}
-                           onChange={setMondayClosingTime}
-                           isDisabled={!operatingHours?.MONDAY?.available}
-                />
-
-            </div>
-            <div className={'flex-centered'}>
-                <LogicalToggle id={'tuesdayAvailable'}
-                               changeHandler={setTuesdayAvailable}
-                               value={operatingHours?.TUESDAY?.available}/>
-                <div>{t('tuesday')}</div>
-                <TimeField name={'tuesdayOpeningTime'}
-                           value={operatingHours?.TUESDAY?.startTime}
-                           onChange={setTuesdayOpeningTime}
-                           isDisabled={!operatingHours?.TUESDAY?.available}
-                />
-                <TimeField name={'tuesdayClosingTime'}
-                           value={operatingHours?.TUESDAY?.endTime}
-                           onChange={setTuesdayClosingTime}
-                           isDisabled={!operatingHours?.TUESDAY?.available}
-                />
-            </div>
-            <div className={'flex-centered'}>
-                <LogicalToggle id={'wednesdayAvailable'}
-                               changeHandler={setWednesdayAvailable}
-                               value={operatingHours?.WEDNESDAY?.available}/>
-                <div>{t('wednesday')}</div>
-                <TimeField name={'wednesdayOpeningTime'}
-                           value={operatingHours?.WEDNESDAY?.startTime}
-                           onChange={setWednesdayOpeningTime}
-                           isDisabled={!operatingHours?.WEDNESDAY?.available}
-                />
-                <TimeField name={'wednesdayClosingTime'}
-                           value={operatingHours?.WEDNESDAY?.endTime}
-                           onChange={setWednesdayClosingTime}
-                           isDisabled={!operatingHours?.WEDNESDAY?.available}
-                />
-            </div>
-            <div className={'flex-centered'}>
-                <LogicalToggle id={'thursdayAvailable'}
-                               changeHandler={setThursdayAvailable}
-                               value={operatingHours?.THURSDAY?.available}/>
-                <div>{t('thursday')}</div>
-                <TimeField name={'thursdayOpeningTime'}
-                           value={operatingHours?.THURSDAY?.startTime}
-                           onChange={setThursdayOpeningTime}
-                           isDisabled={!operatingHours?.THURSDAY?.available}
-                />
-                <TimeField name={'thursdayClosingTime'}
-                           value={operatingHours?.THURSDAY?.endTime}
-                           onChange={setThursdayClosingTime}
-                           isDisabled={!operatingHours?.THURSDAY?.available}
-                />
-            </div>
-            <div className={'flex-centered'}>
-                <LogicalToggle id={'fridayAvailable'}
-                               changeHandler={setFridayAvailable}
-                               value={operatingHours?.FRIDAY?.available}/>
-                <div>{t('friday')}</div>
-                <TimeField name={'fridayOpeningTime'}
-                           value={operatingHours?.FRIDAY?.startTime}
-                           onChange={setFridayOpeningTime}
-                           isDisabled={!operatingHours?.FRIDAY?.available}
-                />
-                <TimeField name={'fridayClosingTime'}
-                           value={operatingHours?.FRIDAY?.endTime}
-                           onChange={setFridayClosingTime}
-                           isDisabled={!operatingHours?.FRIDAY?.available}
-                />
-            </div>
-            <div className={'flex-centered'}>
-                <LogicalToggle id={'saturdayAvailable'}
-                               changeHandler={setSaturdayAvailable}
-                               value={operatingHours?.SATURDAY?.available}/>
-                <div>{t('saturday')}</div>
-                <TimeField name={'saturdayOpeningTime'}
-                           value={operatingHours?.SATURDAY?.startTime}
-                           onChange={setSaturdayOpeningTime}
-                           isDisabled={!operatingHours?.SATURDAY?.available}
-                />
-                <TimeField name={'saturdayClosingTime'}
-                           value={operatingHours?.SATURDAY?.endTime}
-                           onChange={setSaturdayClosingTime}
-                           isDisabled={!operatingHours?.SATURDAY?.available}
-                />
-            </div>
-            <div className={'flex-centered'}>
-                <LogicalToggle id={'sundayAvailable'}
-                               changeHandler={setSundayAvailable}
-                               value={operatingHours?.SUNDAY?.available}/>
-                <div>{t('sunday')}</div>
-                <TimeField name={'sundayOpeningTime'}
-                           value={operatingHours?.SUNDAY?.startTime}
-                           onChange={setSundayOpeningTime}
-                           isDisabled={!operatingHours?.SUNDAY?.available}
-                />
-                <TimeField name={'sundayClosingTime'}
-                           value={operatingHours?.SUNDAY?.endTime}
-                           onChange={setSundayClosingTime}
-                           isDisabled={!operatingHours?.SUNDAY?.available}
-                />
-            </div>
+            <Container>
+                <div className={'form-group-header'}>
+                    {t('openingHours')}
+                </div>
+                <DayOpeningHoursRow objKey={'MONDAY'}
+                                    operatingHours={operatingHours}
+                                    availableHandler={() => dispatch(setMondayAvailable(!operatingHours.MONDAY.available))}
+                                    closingTimeHandler={setMondayClosingTime}
+                                    openingTimeHandler={setMondayOpeningTime}/>
+                <DayOpeningHoursRow objKey={'TUESDAY'}
+                                    operatingHours={operatingHours}
+                                    availableHandler={() => dispatch(setTuesdayAvailable(!operatingHours.TUESDAY.available))}
+                                    closingTimeHandler={setTuesdayClosingTime}
+                                    openingTimeHandler={setTuesdayOpeningTime}/>
+                <DayOpeningHoursRow objKey={'WEDNESDAY'}
+                                    operatingHours={operatingHours}
+                                    availableHandler={() => dispatch(setWednesdayAvailable(!operatingHours.WEDNESDAY.available))}
+                                    closingTimeHandler={setWednesdayClosingTime}
+                                    openingTimeHandler={setWednesdayOpeningTime}/>
+                <DayOpeningHoursRow objKey={'THURSDAY'}
+                                    operatingHours={operatingHours}
+                                    availableHandler={() => dispatch(setThursdayAvailable(!operatingHours.THURSDAY.available))}
+                                    closingTimeHandler={setThursdayClosingTime}
+                                    openingTimeHandler={setThursdayOpeningTime}/>
+                <DayOpeningHoursRow objKey={'FRIDAY'}
+                                    operatingHours={operatingHours}
+                                    availableHandler={() => dispatch(setFridayAvailable(!operatingHours.FRIDAY.available))}
+                                    closingTimeHandler={setFridayClosingTime}
+                                    openingTimeHandler={setFridayOpeningTime}/>
+                <DayOpeningHoursRow objKey={'SATURDAY'}
+                                    operatingHours={operatingHours}
+                                    availableHandler={() => dispatch(setSaturdayAvailable(!operatingHours.SATURDAY.available))}
+                                    closingTimeHandler={setSaturdayClosingTime}
+                                    openingTimeHandler={setSaturdayOpeningTime}/>
+                <DayOpeningHoursRow objKey={'SUNDAY'}
+                                    operatingHours={operatingHours}
+                                    availableHandler={() => dispatch(setSundayAvailable(!operatingHours.SUNDAY.available))}
+                                    closingTimeHandler={setSundayClosingTime}
+                                    openingTimeHandler={setSundayOpeningTime}/>
+            </Container>
         </>
     );
 }
