218e57963ac3746711478725a48397680e692b4f
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-10-11 17:54:12 +0200

    Update TranslationRecordsHeader.jsx

--- src/app/hungryscan/cms/translations/TranslationRecordsHeader.jsx
+++ src/app/hungryscan/cms/translations/TranslationRecordsHeader.jsx
@@ -5,32 +5,50 @@ import {CustomNoOptionsMessage} from "../form-components/CustomNoOptionsMessage"
 import {useTranslation} from "react-i18next";
 import {setChosenGroup} from "../../../../slices/translationsSlice";
 import {useDispatch, useSelector} from "react-redux";
+import {getTranslation} from "../../../../locales/langUtils";
 
 export const TranslationRecordsHeader = () => {
     const {t} = useTranslation();
     const dispatch = useDispatch();
     const options = [
         {
-            value: 'dishesCategories',
+            value: 'categoriesMenuItems',
             label: t('dishesCategories')
         },
         {
-            value: 'variants',
-            label: t('variants')
+            value: 'menuItemsVariants',
+            label: t('menuItemsVariants')
         },
         {
             value: 'additions',
             label: t('additions')
+        },
+        {
+            value: 'welcomeSlogans',
+            label: t('welcomeSlogans')
         }
     ];
     const {chosenGroup} = useSelector(state => state.translations.view);
 
     useEffect(() => {
         dispatch(setChosenGroup({
-            value: 'dishesCategories',
+            value: 'categoriesMenuItems',
             label: t('dishesCategories')
         }));
-    }, []);
+    }, [dispatch, t]);
+
+    const renderMiddleHeader = () => {
+        if(chosenGroup?.value === 'categoriesMenuItems') {
+            return (
+                <span className={'translations-records-column middle header'}>{t('description')}</span>
+            );
+        } else if (chosenGroup?.value === 'welcomeSlogans') {
+            return (
+                <span className={'translations-records-column middle header'}>{t('welcomeSlogan')}</span>
+            );
+        }
+        return (<></>);
+    }
 
     return (
         <header className={'translations-vertical-split-header-left'}>
@@ -45,8 +63,10 @@ export const TranslationRecordsHeader = () => {
                     components={{NoOptionsMessage: CustomNoOptionsMessage}}
             />
             <div className={'translation-record-grid header'}>
-                <span className={'translations-records-column left header'}>{t('name')}</span>
-                <span className={'translations-records-column middle header'}>{t('description')}</span>
+                <span className={'translations-records-column left header'}>
+                    {chosenGroup?.value !== 'welcomeSlogans' ? t('name') : t('menuName')}
+                </span>
+                {renderMiddleHeader()}
                 <span className={'translations-records-column right header'}>{t('status')}</span>
             </div>
         </header>
