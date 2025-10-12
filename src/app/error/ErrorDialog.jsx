4f2ac697c791bd229919361e2ffcdb486977acaa
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-10-12 08:30:55 +0200

    Updated ErrorDialog.jsx

--- src/app/error/ErrorDialog.jsx
+++ src/app/error/ErrorDialog.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {useTranslation} from "react-i18next";
 
-export const ErrorDialog = ({ error, errorInfo }) => {
+export const ErrorDialog = ({error, errorInfo}) => {
     const [open, setOpen] = React.useState(true);
     const {t} = useTranslation();
 
