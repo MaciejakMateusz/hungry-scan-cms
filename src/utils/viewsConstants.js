c176058789a0730d3840ee34d751ef20f9c77725
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-10-12 08:29:54 +0200

    Updated viewsConstants.js

--- src/utils/viewsConstants.js
+++ src/utils/viewsConstants.js
@@ -1,14 +1,12 @@
 //Dashboard views:
+export const USER_PROFILE = "dashboard/userProfile";
 export const STATS = "dashboard/stats";
-export const D_CODE_QR = "dashboard/codeQr";
+export const CODE_QR = "dashboard/codeQr";
 export const PACKAGE = "dashboard/package";
 export const USERS = "dashboard/users";
 
 //CMS views:
 export const DISHES_CATEGORIES = "cms/dishesCategories";
-export const VARIANTS = "cms/variants";
 export const ADDITIONS = "cms/additions";
-export const C_CODE_QR = "cms/codeQr";
 export const TRANSLATIONS = "cms/translations";
-export const AD_POP_UPS = "cms/adPopUps";
 export const PERSONALIZATION = "cms/personalization";
