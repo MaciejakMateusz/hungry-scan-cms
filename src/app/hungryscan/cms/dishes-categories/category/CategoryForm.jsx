2a64212f797014e7a31337569b4b386ea6820df1
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-10-12 08:32:31 +0200

    Updated CategoryForm.jsx

--- src/app/hungryscan/cms/dishes-categories/category/CategoryForm.jsx
+++ src/app/hungryscan/cms/dishes-categories/category/CategoryForm.jsx
@@ -1,18 +1,14 @@
 import React from "react";
-import {useTranslation} from "react-i18next";
 import {CategoryFormTemplate} from "./CategoryFormTemplate";
 import {useDispatch, useSelector} from "react-redux";
 import {setErrorMessage} from "../../../../../slices/categoryFormSlice";
 import {FormErrorDialog} from "../../../../error/FormErrorDialog";
-import {LoadingSpinner} from "../../../../icons/LoadingSpinner";
 import {FormHeader} from "../../shared-components/FormHeader";
 
 export const CategoryForm = ({formHeader, onFormDiscard, onFormSubmit}) => {
-    const {t} = useTranslation();
     const dispatch = useDispatch();
     const {errorData, errorMessage} = useSelector(state => state.categoryForm.form);
     const {isLoading} = useSelector(state => state.categoryForm.postCategory);
-
 
     return (
         <div className={'background'}>
@@ -26,12 +22,7 @@ export const CategoryForm = ({formHeader, onFormDiscard, onFormSubmit}) => {
                 />
                 <div className={'form-grid category'}>
                     <form className={'padded-form-fragment'}>
-                        <div className={'form-header'}>
-                            {formHeader}
-                        </div>
-                        <div className={'padded-form-container'}>
-                            <CategoryFormTemplate/>
-                        </div>
+                        <CategoryFormTemplate/>
                     </form>
                 </div>
             </div>
