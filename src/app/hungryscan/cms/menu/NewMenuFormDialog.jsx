e7547882b693824d011b007f9fef34c9d42d15b4
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-10-26 11:14:59 +0100

    Updated NewMenuFormDialog.jsx

--- src/app/hungryscan/cms/menu/NewMenuFormDialog.jsx
+++ src/app/hungryscan/cms/menu/NewMenuFormDialog.jsx
@@ -1,27 +1,43 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {useTranslation} from "react-i18next";
 import {useDispatch, useSelector} from "react-redux";
-import {clearForm, postMenu, setErrorData, setNewMenuCreated, setNewMenuFormActive} from "../../../../slices/menuSlice";
+import {
+    clearForm,
+    postMenu,
+    setColor,
+    setErrorData,
+    setNewMenuCreated,
+    setNewMenuFormActive
+} from "../../../../slices/menuSlice";
 import {fetchActiveMenu} from "../../../../slices/cmsSlice";
 import {useConfirmationMessage} from "../../../../hooks/useConfirmationMessage";
 import {MenuFormTemplate} from "./MenuFormTemplate";
+import {FormErrorDialog} from "../../../error/FormErrorDialog";
 
 export const NewMenuFormDialog = () => {
     const {t} = useTranslation();
     const dispatch = useDispatch();
     const {menu} = useSelector(state => state.menu.fetchMenu);
-    const {name} = useSelector(state => state.menu.form);
-    const {errorData, isLoading} = useSelector(state => state.menu.postMenu);
+    const {menuColors} = useSelector(state => state.menu.fetchMenuColors);
+    const {name, color, errorData} = useSelector(state => state.menu.form);
+    const {isLoading} = useSelector(state => state.menu.postMenu);
     const renderConfirmation = useConfirmationMessage(setNewMenuCreated);
+
+    useEffect(() => {
+        dispatch(setColor(menuColors[8] ?? {id: 9, hex: '#9746FF'}));
+    }, [dispatch, menuColors]);
 
     const handleFormSubmit = async (e) => {
         e.preventDefault();
-        const resultAction = await dispatch(postMenu({menu: {id: menu?.id, name: name}}));
+        const resultAction = await dispatch(postMenu({menu: {id: menu?.id, name: name, color: color}}));
         if (postMenu.fulfilled.match(resultAction)) {
             await dispatch(fetchActiveMenu());
             await dispatch(setNewMenuFormActive(false));
             dispatch(clearForm());
+            dispatch(setErrorData(null));
             renderConfirmation();
+        } else {
+            dispatch(setErrorData(resultAction?.payload));
         }
     }
 
@@ -32,10 +48,13 @@ export const NewMenuFormDialog = () => {
     }
 
     return (
-        <MenuFormTemplate formHeader={t('addMenu')}
-                          submitHandler={handleFormSubmit}
-                          discardHandler={handleFormDiscard}
-                          errorData={errorData}
-                          isLoading={isLoading}/>
+        <>
+            <FormErrorDialog errorData={errorData} setErrorData={setErrorData}/>
+            <MenuFormTemplate formHeader={t('addMenu')}
+                              submitHandler={handleFormSubmit}
+                              discardHandler={handleFormDiscard}
+                              errorData={errorData}
+                              isLoading={isLoading}/>
+        </>
     );
 }
