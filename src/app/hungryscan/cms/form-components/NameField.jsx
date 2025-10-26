7730ccb4f2e5ebaa24e48d48d6ec68c642a6b603
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-10-26 11:14:39 +0100

    Updated NameField.jsx

--- src/app/hungryscan/cms/form-components/NameField.jsx
+++ src/app/hungryscan/cms/form-components/NameField.jsx
@@ -16,25 +16,30 @@ export const NameField = (props) => {
     }, [props.error, props.value]);
 
     return (
-        <div className={'form-field-wrapper'}>
-            <div className={'form-field-container'}>
-                <label htmlFor={props.id} className={'form-label'}>
-                    <InformationTooltip text={t('nameFieldTooltip')}/>
-                    {t('name')} *
-                </label>
-                <input type={'text'}
-                       className={`form-field name ${hasError ? 'error' : ''}`}
-                       id={props.id}
-                       name={'name'}
-                       value={props.value}
-                       onChange={(e) => {
-                           props.onChange(e.target.value)
-                           resetErrorStyles()
-                       }}
-                       placeholder={isFocused ? '' : t('addName')}
-                       onFocus={() => setIsFocused(true)}
-                       onBlur={() => setIsFocused(false)}/>
+        <>
+            <div className={'form-field-wrapper'}>
+                <div className={'form-field-container'}>
+                    <label htmlFor={props.id} className={'form-label'}>
+                        <InformationTooltip text={t('nameFieldTooltip')}/>
+                        {t('name')} *
+                    </label>
+                    <div className={'full-width'}>
+                        <input type={'text'}
+                               className={`form-field name ${hasError && 'error'}`}
+                               id={props.id}
+                               name={'name'}
+                               value={props.value}
+                               onChange={(e) => {
+                                   props.onChange(e.target.value)
+                                   resetErrorStyles()
+                               }}
+                               placeholder={isFocused ? '' : t('addName')}
+                               onFocus={() => setIsFocused(true)}
+                               onBlur={() => setIsFocused(false)}/>
+                        {hasError && <div className={'validation-msg'}>{props.error?.name}</div>}
+                    </div>
+                </div>
             </div>
-        </div>
+        </>
     );
 }
