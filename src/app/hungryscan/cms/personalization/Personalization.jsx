ad1d952cbbbebc712c84eb8f5e058839c88fcafd
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-10-26 11:15:50 +0100

    Updated Personalization.jsx

--- src/app/hungryscan/cms/personalization/Personalization.jsx
+++ src/app/hungryscan/cms/personalization/Personalization.jsx
@@ -2,18 +2,19 @@ import React from "react";
 import {useTranslation} from "react-i18next";
 import {PersonalizationForm} from "./form/PersonalizationForm";
 import {useDispatch, useSelector} from "react-redux";
-import {postPersonalization, setPersonalizationUpdated} from "../../../../slices/personalizationSlice";
+import {postPersonalization, setErrorData, setPersonalizationUpdated} from "../../../../slices/personalizationSlice";
 import {useConfirmationMessage} from "../../../../hooks/useConfirmationMessage";
 import {useFetchCurrentRestaurant} from "../../../../hooks/useFetchCurrentRestaurant";
 import {fetchActiveMenu} from "../../../../slices/cmsSlice";
 import {FormHeader} from "../shared-components/FormHeader";
+import {FormErrorDialog} from "../../../error/FormErrorDialog";
 
 export const Personalization = () => {
     const {t} = useTranslation();
     const dispatch = useDispatch();
     const renderConfirmation = useConfirmationMessage(setPersonalizationUpdated);
     const {activeMenu} = useSelector(state => state.globalParams.globalParams);
-    const {isLoading} = useSelector(state => state.personalization.postPersonalization);
+    const {isLoading, errorData} = useSelector(state => state.personalization.postPersonalization);
     const fetchCurrentRestaurant = useFetchCurrentRestaurant()
 
     const handleFormSubmit = async (e) => {
@@ -22,16 +23,21 @@ export const Personalization = () => {
         if (postPersonalization.fulfilled.match(resultAction)) {
             await fetchCurrentRestaurant();
             await dispatch(fetchActiveMenu());
+            dispatch(setErrorData(null));
             renderConfirmation();
+        } else {
+            dispatch(setErrorData(resultAction.payload));
         }
     }
 
     return (
         <div className={'background'}>
+            <FormErrorDialog errorData={errorData} setErrorData={setErrorData}/>
             <form className={'cms-padded-view-container'}>
                 <FormHeader formHeader={t('personalization')}
                             onFormSubmit={handleFormSubmit}
                             isLoading={isLoading}
+                            submitDisabled={errorData}
                 />
                 <div className={'form-grid'}>
                     <div className={'padded-form-fragment'}>
