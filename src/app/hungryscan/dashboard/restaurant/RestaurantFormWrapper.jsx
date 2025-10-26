1a6bbd02bfe2bcd4eb127fef6856fc0c98790ddb
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-10-26 11:16:52 +0100

    Updated RestaurantFormWrapper.jsx

--- src/app/hungryscan/dashboard/restaurant/RestaurantFormWrapper.jsx
+++ src/app/hungryscan/dashboard/restaurant/RestaurantFormWrapper.jsx
@@ -1,20 +1,18 @@
 import React from "react";
-import {useDispatch, useSelector} from "react-redux";
+import {useSelector} from "react-redux";
 import {RestaurantFormTemplate} from "./RestaurantFormTemplate";
 import {FormErrorDialog} from "../../../error/FormErrorDialog";
-import {setErrorMessage,} from "../../../../slices/restaurantSlice";
 import {FormHeader} from "../../cms/shared-components/FormHeader";
+import {setErrorData} from "../../../../slices/restaurantSlice";
 
 export const RestaurantFormWrapper = ({onSubmit, onDiscard, title}) => {
-    const dispatch = useDispatch();
-    const {errorMessage} = useSelector(state => state.restaurant.form);
-    const {errorData, isLoading} = useSelector(state => state.restaurant.post);
+    const {isLoading} = useSelector(state => state.restaurant.post);
+    const {errorData} = useSelector(state => state.restaurant.form);
 
     return (
         <div className={'background'}>
             <div className={'cms-padded-view-container'}>
-                {errorMessage ?
-                    <FormErrorDialog error={errorData} resetMessage={() => dispatch(setErrorMessage(null))}/> : null}
+                <FormErrorDialog errorData={errorData} setErrorData={setErrorData}/>
                 <FormHeader formHeader={title}
                             onFormSubmit={onSubmit}
                             onFormDiscard={onDiscard}
