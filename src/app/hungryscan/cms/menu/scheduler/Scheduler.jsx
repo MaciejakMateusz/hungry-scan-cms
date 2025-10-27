31c354c587cb9e42c2a5eb762558329b31f59c10
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-10-27 19:23:07 +0100

    Updated Scheduler.jsx

--- src/app/hungryscan/cms/menu/scheduler/Scheduler.jsx
+++ src/app/hungryscan/cms/menu/scheduler/Scheduler.jsx
@@ -2,9 +2,12 @@ import React, {useEffect, useMemo, useRef, useState} from "react";
 import {useSelector} from "react-redux";
 import {SchedulerControlPanel} from "./SchedulerControlPanel";
 import {SchedulerCalendar} from "./SchedulerCalendar";
+import {FormErrorDialog} from "../../../../error/FormErrorDialog";
+import {setUpdatingPlansError} from "../../../../../slices/menuSlice";
 
 export const Scheduler = () => {
     const {restaurant} = useSelector((state) => state.dashboard.view);
+    const {updatingPlansError} = useSelector((state) => state.menu.updatePlans);
     const externalRef = useRef(null);
     const trashRef = useRef(null);
 
@@ -20,6 +23,7 @@ export const Scheduler = () => {
 
     return (
         <div className={'background'}>
+            <FormErrorDialog errorData={updatingPlansError} setErrorData={setUpdatingPlansError}/>
             <div className={'scheduler-wrapper'}>
                 <div className={'scheduler-flex'}>
                     <SchedulerControlPanel menusConfig={menusConfig}
