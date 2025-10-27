739621fdd86bef63f2258f1c6b2760b7d1824eb8
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-10-27 19:21:19 +0100

    Created CookieConsent.style.js

---
+++ src/app/main/cookies/CookieConsent.style.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    position: fixed;
+    bottom: 40px;
+    left: 40px;
+    height: 200px;
+    width: 400px;
+    background: var(--Basic-300);
+    box-shadow: 0 0 25px 0 rgba(0, 0, 0, 0.05);
+    padding: 20px;
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+`
+
+export const Header = styled.span`
+    color: #000;
+    font-size: 1.2rem;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+export const Text = styled.span`
+    color: var(--Grey-700);
+    font-size: 1rem;
+`
+
+export const Footer = styled.footer`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+`
