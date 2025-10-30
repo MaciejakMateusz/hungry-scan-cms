4bdae9bc0b313096e5f0e57938455cb2e736be76
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-10-30 21:47:30 +0100

    Updated UserForm.jsx

--- src/app/hungryscan/dashboard/users/form/UserForm.jsx
+++ src/app/hungryscan/dashboard/users/form/UserForm.jsx
@@ -1,14 +1,20 @@
 import React, {useEffect} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {FormErrorDialog} from "../../../../error/FormErrorDialog";
-import {getOrganizationRestaurants, getRoles, setErrorData} from "../../../../../slices/usersSlice";
+import {
+    getOrganizationRestaurants,
+    getRoles,
+    setSaveUserError,
+    setUpdateUserError
+} from "../../../../../slices/usersSlice";
 import {UserFormTemplate} from "./UserFormTemplate";
 import {FormHeader} from "../../../cms/shared-components/FormHeader";
 
 export const UserForm = ({formHeader, onFormDiscard, onFormSubmit}) => {
     const dispatch = useDispatch();
-    const {errorData} = useSelector(state => state.users.form);
     const {isLoading} = useSelector(state => state.users.saveUser);
+    const {saveUserError} = useSelector(state => state.users.saveUser);
+    const {updateUserError} = useSelector(state => state.users.updateUser);
 
     useEffect(() => {
         dispatch(getRoles());
@@ -18,8 +24,8 @@ export const UserForm = ({formHeader, onFormDiscard, onFormSubmit}) => {
     return (
         <div className={'background'}>
             <div className={'cms-padded-view-container'}>
-                {errorData && <FormErrorDialog error={errorData}
-                                                  resetMessage={() => dispatch(setErrorData(null))}/>}
+                <FormErrorDialog errorData={saveUserError} setErrorData={setSaveUserError}/>
+                <FormErrorDialog errorData={updateUserError} setErrorData={setUpdateUserError}/>
                 <FormHeader formHeader={formHeader}
                             onFormSubmit={onFormSubmit}
                             onFormDiscard={onFormDiscard}
