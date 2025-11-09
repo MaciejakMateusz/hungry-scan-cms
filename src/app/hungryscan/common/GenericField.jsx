b8f4490e7613d1446f87b377796f21570ff4c1a2
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-11-09 16:38:47 +0100

    Updated GenericField.jsx

--- src/app/hungryscan/common/GenericField.jsx
+++ src/app/hungryscan/common/GenericField.jsx
@@ -1,17 +1,14 @@
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import {InformationTooltip} from "../cms/shared-components/InformationTooltip";
+import {useDispatch} from "react-redux";
 
 export const GenericField = (props) => {
+    const dispatch = useDispatch();
     const [isFocused, setIsFocused] = useState(false);
-    const [hasError, setHasError] = useState(props.error);
 
     const resetErrorStyles = () => {
-        setHasError(null);
+        dispatch(props.errorSetter({...props.error, [props.id]: null}))
     }
-
-    useEffect(() => {
-        setHasError(props.error);
-    }, [props.error]);
 
     return (
         <div className={'form-field-wrapper'}>
@@ -22,21 +19,21 @@ export const GenericField = (props) => {
                 </label>
                 <div className={'full-width'}>
                     <input type={props.type}
-                           className={`form-field name ${hasError ? 'error' : ''}`}
+                           className={`form-field name ${props.error?.[props.id] ? 'error' : ''}`}
                            id={props.id}
                            name={props.id}
                            readOnly={props.readOnly}
                            disabled={props.disabled}
-                           value={props.value}
+                           value={props.value ?? ''}
                            onChange={(e) => {
-                               props.onChange(e.target.value)
-                               resetErrorStyles()
+                               props.onChange(e.target.value);
+                               resetErrorStyles();
                            }}
-                           placeholder={!isFocused && props.placeholder}
+                           placeholder={!isFocused ? props.placeholder : ''}
                            onFocus={() => setIsFocused(true)}
                            onBlur={() => setIsFocused(false)}
                     />
-                    {hasError && <div className={'validation-msg'}>{props.error}</div>}
+                    {props.error?.[props.id] && <div className={'validation-msg'}>{props.error[props.id]}</div>}
                 </div>
             </div>
         </div>
