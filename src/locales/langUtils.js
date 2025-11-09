edd94819ce19440ffeb34c12c937ba912f801057
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-11-09 16:39:50 +0100

    Updated langUtils.js

--- src/locales/langUtils.js
+++ src/locales/langUtils.js
@@ -1,53 +1,15 @@
 import i18n from "i18next";
 import {getCookie} from "../utils/utils";
 
-export const getTranslation = (obj) => {
-    if (typeof obj === 'string') {
-        return obj;
-    }
-
-    if (!obj) {
-        return '';
-    }
-
-    const lang = getLanguage();
-    switch (lang) {
-        case 'pl': return obj.pl;
-        case 'pl-PL': return obj.pl;
-        case 'en': return obj.en ? obj.en : obj.pl;
-        case 'fr': return obj.fr ? obj.fr : obj.pl;
-        case 'de': return obj.de ? obj.de : obj.pl;
-        case 'es': return obj.es ? obj.es : obj.pl;
-        case 'uk': return obj.uk ? obj.uk : obj.pl;
-        default: return '';
-    }
-};
-
 export const getLanguage = () => {
     const lngCookie = getCookie('lng');
     return lngCookie ? lngCookie : i18n.language;
 }
 
-export const updateTranslatable = (translatable, translation) => {
-    const currentLanguage = getLanguage();
-    const languageKey = currentLanguage === 'pl-PL' ? 'pl' : currentLanguage;
-    const defaultLanguageIsNull = !translatable[process.env.REACT_APP_DEFAULT_LANGUAGE];
-    if (defaultLanguageIsNull) {
-        return {
-            ...translatable,
-            [process.env.REACT_APP_DEFAULT_LANGUAGE]: translation,
-            [languageKey]: translation
-        }
-    }
-    return {...translatable, [languageKey]: translation};
+export const updateTranslatable = (translatable, translation, restaurantLanguage) => {
+    return {...translatable, [restaurantLanguage]: translation};
 }
 
-export const createTranslatable = translation => {
-    const currentLanguage = getLanguage();
-    const languageKey = currentLanguage === 'pl-PL' ? 'pl' : currentLanguage;
-    return {
-        id: null,
-        [process.env.REACT_APP_DEFAULT_LANGUAGE]: translation,
-        [languageKey]: translation
-    };
+export const createTranslatable = (translation, restaurantLanguage) => {
+    return {id: null, [restaurantLanguage]: translation};
 }
