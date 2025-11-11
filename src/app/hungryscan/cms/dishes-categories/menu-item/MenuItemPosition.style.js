0ad509b30f0f12e1661d8b05947e1eae954affc4
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-11-11 12:21:42 +0100

    Created MenuItemPosition.style.js

---
+++ src/app/hungryscan/cms/dishes-categories/menu-item/MenuItemPosition.style.js
@@ -0,0 +1,60 @@
+import styled, {css} from "styled-components";
+
+export const DragAndDropWrapper = styled.div`
+    grid-column: 1;
+    display: flex;
+    gap: 10px;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    padding-left: 20px;
+    cursor: grab;
+
+    ${({$disabled}) =>
+            $disabled &&
+            css`
+                cursor: unset;
+                color: #7d7d7d;
+            `}
+`;
+
+export const ImageWrapper = styled.div`
+    grid-column: 2;
+`;
+
+export const TextWrapper = styled.div`
+    grid-column: 3;
+    max-width: 340px;
+    width: 100%;
+`;
+
+export const PricesWrapper = styled.div`
+    grid-column: 4;
+    color: #33353E;
+`;
+
+export const BannersWrapper = styled.div`
+    grid-column: 5;
+    color: #33353E;
+    min-width: 68px;
+`;
+
+export const VariantsAdditionsCounters = styled.div`
+    grid-column: 6;
+`;
+
+export const LabelsAllergensCounters = styled.div`
+    grid-column: 7;
+`;
+
+
+export const Banner = styled.div`
+    padding: 6px 10px;
+    border-radius: 16px;
+    background: #E0F3FF;
+    color: #016DFF;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 5px;
+`
