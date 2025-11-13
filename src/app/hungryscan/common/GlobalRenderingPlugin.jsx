db2da9d6433d4c9b496049851c5e2ad0f03e9adb
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-11-13 21:30:53 +0100

    Updated GlobalRenderingPlugin.jsx

--- src/app/hungryscan/common/GlobalRenderingPlugin.jsx
+++ src/app/hungryscan/common/GlobalRenderingPlugin.jsx
@@ -1,4 +1,12 @@
 import React from "react";
 import {ConfirmationMessagesRenderer} from "./ConfirmationMessagesRenderer";
+import {DetailsDialog} from "../cms/dialog-windows/DetailsDialog";
 
-export const GlobalRenderingPlugin = () => <ConfirmationMessagesRenderer/>;
+export const GlobalRenderingPlugin = () => {
+    return (
+        <>
+            <ConfirmationMessagesRenderer/>
+            <DetailsDialog/>
+        </>
+    );
+};
