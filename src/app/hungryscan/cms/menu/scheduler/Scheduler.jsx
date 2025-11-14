e0252fe444d6a677caef131413c4c9deee4fbf05
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-11-14 21:21:24 +0100

    Updated Scheduler.jsx

--- src/app/hungryscan/cms/menu/scheduler/Scheduler.jsx
+++ src/app/hungryscan/cms/menu/scheduler/Scheduler.jsx
@@ -1,18 +1,32 @@
 import React, {useEffect, useMemo, useRef, useState} from "react";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {SchedulerControlPanel} from "./SchedulerControlPanel";
 import {SchedulerCalendar} from "./SchedulerCalendar";
 import {FormErrorDialog} from "../../../../error/FormErrorDialog";
-import {setUpdatingPlansError} from "../../../../../slices/menuSlice";
+import {setPlansUpdated, setUpdatingPlansError, updatePlans} from "../../../../../slices/menuSlice";
+import {FormHeader} from "../../shared-components/FormHeader";
+import {useTranslation} from "react-i18next";
+import {fetchActiveMenu, setSchedulerActive} from "../../../../../slices/cmsSlice";
+import {useFetchCurrentRestaurant} from "../../../../../hooks/useFetchCurrentRestaurant";
+import {useConfirmationMessage} from "../../../../../hooks/useConfirmationMessage";
 
 export const Scheduler = () => {
+    const {t} = useTranslation();
+    const dispatch = useDispatch();
     const {restaurant} = useSelector((state) => state.dashboard.view);
     const {updatingPlansError} = useSelector((state) => state.menu.updatePlans);
     const externalRef = useRef(null);
-    const trashRef = useRef(null);
+    const getRestaurant = useFetchCurrentRestaurant();
+    const renderConfirmation = useConfirmationMessage(setPlansUpdated);
+    const {isLoading} = useSelector((state) => state.menu.updatePlans);
 
     const initialMenus = useMemo(() => {
-        return [...(restaurant?.value.menus || [])]
+        return [...(restaurant?.value.menus || [])].sort((a, b) => {
+            if (a.standard !== b.standard) {
+                return a.standard ? -1 : 1;
+            }
+            return a.name.localeCompare(b.name, 'pl', { sensitivity: 'base' });
+        });
     }, [restaurant.value.menus]);
 
     const [menusConfig, setMenusConfig] = useState(initialMenus);
@@ -21,19 +35,40 @@ export const Scheduler = () => {
         setMenusConfig(initialMenus);
     }, [initialMenus]);
 
+    const handleDiscard = () => {
+        dispatch(setSchedulerActive(false));
+        dispatch(setUpdatingPlansError(null));
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const resultAction = await dispatch(updatePlans({menus: menusConfig}));
+        if (updatePlans.fulfilled.match(resultAction)) {
+            await getRestaurant();
+            await dispatch(fetchActiveMenu());
+            dispatch(setSchedulerActive(false));
+            renderConfirmation();
+        } else {
+            dispatch(setUpdatingPlansError(resultAction?.payload));
+        }
+    };
+
     return (
         <div className={'background'}>
             <FormErrorDialog errorData={updatingPlansError} setErrorData={setUpdatingPlansError}/>
             <div className={'scheduler-wrapper'}>
+                <FormHeader formHeader={t('menuSchedules')}
+                            onFormSubmit={handleSubmit}
+                            onFormDiscard={handleDiscard}
+                            isLoading={isLoading}
+                />
                 <div className={'scheduler-flex'}>
                     <SchedulerControlPanel menusConfig={menusConfig}
                                            setMenusConfig={setMenusConfig}
-                                           externalRef={externalRef}
-                                           trashRef={trashRef}/>
+                                           externalRef={externalRef}/>
                     <SchedulerCalendar menusConfig={menusConfig}
                                        setMenusConfig={setMenusConfig}
-                                       externalRef={externalRef}
-                                       trashRef={trashRef}/>
+                                       externalRef={externalRef}/>
                 </div>
             </div>
         </div>
