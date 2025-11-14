a761529bcd21acb8af0eea2d6dd6bdb67d84d849
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-11-14 21:23:42 +0100

    Updated TranslationsList.jsx

--- src/app/hungryscan/cms/translations/TranslationsList.jsx
+++ src/app/hungryscan/cms/translations/TranslationsList.jsx
@@ -1,111 +1,122 @@
 import React from "react";
 import {TranslationRecord} from "./TranslationRecord";
-import {setActiveRecord, setActiveRecordId} from "../../../../slices/translationsSlice";
 import {useTranslation} from "react-i18next";
 import {useDispatch, useSelector} from "react-redux";
+import {TranslationContainer} from "./TranslationsList.style";
+import {setActiveRecord, setActiveRecordId} from "../../../../slices/translationsSlice";
 
 export const TranslationsList = () => {
     const {t} = useTranslation();
+    const dispatch = useDispatch();
     const {dishesCategories, variants, additions, menus} = useSelector(state => state.translations.view);
     const {chosenGroup} = useSelector(state => state.translations.view);
-    const dispatch = useDispatch();
 
     const renderRecords = () => {
         switch (chosenGroup?.value) {
             case 'menuItemsVariants':
                 return (
                     <>
-                        {variants?.length > 0 ? variants.map((menuItem, index) => (
-                            <div key={menuItem.id}>
-                                <TranslationRecord parent={true}
-                                                   index={index + 1}
-                                                   record={menuItem}
+                        {variants?.length > 0 ? variants.map(menuItem => (
+                            <TranslationContainer key={menuItem.id} $hasParent={true}>
+                                <TranslationRecord record={menuItem}
+                                                   isParent={true}
+                                                   parentTranslatableKey={'name'}
+                                                   childrenKey={'variants'}
                                                    setActive={() => {
                                                        dispatch(setActiveRecordId('p' + menuItem.id));
                                                        dispatch(setActiveRecord(menuItem));
                                                    }}
                                 />
+                                <div className={'draggable-position-separator'}/>
                                 {menuItem.variants && menuItem.variants.map(variant => (
                                     <div key={variant.id}>
-                                        <TranslationRecord parent={false}
-                                                           record={variant}
+                                        <TranslationRecord record={variant}
+                                                           childTranslatableKey={'name'}
                                                            setActive={() => {
                                                                dispatch(setActiveRecordId('c' + variant.id));
                                                                dispatch(setActiveRecord(variant));
                                                            }}
                                         />
+                                        <div className={'draggable-position-separator'}/>
                                     </div>
                                 ))}
-                            </div>
+                            </TranslationContainer>
                         )) : <p className={'flex-centered'}>{t('noRecordsFound')}</p>}
                     </>
                 );
             case 'categoriesMenuItems':
                 return (
                     <>
-                        {dishesCategories?.length > 0 ? dishesCategories.map((category, index) => (
-                            <div key={category.id}>
-                                <TranslationRecord parent={true}
-                                                   index={index + 1}
-                                                   record={category}
+                        {dishesCategories?.length > 0 ? dishesCategories.map((category) => (
+                            <TranslationContainer key={category.id} $hasParent={true}>
+                                <TranslationRecord record={category}
+                                                   isParent={true}
+                                                   childrenKey={'menuItems'}
+                                                   parentTranslatableKey={'name'}
                                                    setActive={() => {
-                                                       dispatch(setActiveRecordId('p' + category.id))
+                                                       dispatch(setActiveRecordId('p' + category.id));
                                                        dispatch(setActiveRecord(category));
                                                    }}
                                 />
+                                <div className={'draggable-position-separator'}/>
                                 {category.menuItems && category.menuItems.map(menuItem => (
                                     <div key={menuItem.id}>
-                                        <TranslationRecord parent={false}
-                                                           record={menuItem}
+                                        <TranslationRecord record={menuItem}
+                                                           childTranslatableKey={'name'}
                                                            setActive={() => {
                                                                dispatch(setActiveRecord(menuItem));
                                                                dispatch(setActiveRecordId('c' + menuItem.id));
                                                            }}
                                         />
+                                        <div className={'draggable-position-separator'}/>
                                     </div>
                                 ))}
-                            </div>
+                            </TranslationContainer>
                         )) : t('noRecordsFound')}
                     </>
                 );
             case 'additions':
                 return (
                     <>
-                        {additions?.length > 0 ? additions.map((addition, index) => (
-                            <div key={addition.id}>
-                                <TranslationRecord parent={false}
-                                                   index={index + 1}
-                                                   record={addition}
+                        {additions?.length > 0 ? additions.map(addition => (
+                            <TranslationContainer key={addition.id} $hasParent={false}>
+                                <TranslationRecord record={addition}
+                                                   childTranslatableKey={'name'}
                                                    setActive={() => {
                                                        dispatch(setActiveRecordId('c' + addition.id))
                                                        dispatch(setActiveRecord(addition));
                                                    }}
                                 />
-                            </div>
+                                <div className={'draggable-position-separator'}/>
+                            </TranslationContainer>
                         )) : t('noRecordsFound')}
                     </>
                 );
             case 'welcomeSlogans':
                 return (
                     <>
-                        {menus?.length > 0 ? menus.map((menu, index) => (
-                            <div key={menu.id}>
-                                <TranslationRecord parent={false}
-                                                   index={index + 1}
-                                                   record={menu}
+                        {menus?.length > 0 ? menus.map(menu => (
+                            <TranslationContainer key={menu.id} $hasParent={false}>
+                                <TranslationRecord record={menu}
+                                                   childTranslatableKey={'message'}
                                                    setActive={() => {
                                                        dispatch(setActiveRecordId('c' + menu.id))
                                                        dispatch(setActiveRecord(menu));
                                                    }}
                                 />
-                            </div>
+                                <div className={'draggable-position-separator'}/>
+                            </TranslationContainer>
                         )) : t('noRecordsFound')}
                     </>
                 );
             default:
-                return (<></>);
+                return (<p>{t('noRecordsFound')}</p>);
         }
     }
 
-    return renderRecords();
+    return (
+        <div className={'scrollable-wrapper'}>
+            {renderRecords()}
+        </div>
+    );
 }
