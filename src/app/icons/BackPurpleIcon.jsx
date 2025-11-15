2f03836548237140098257df5d8e4026e983b7c5
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-11-15 21:48:31 +0100

    Updated BackPurpleIcon.jsx

--- src/app/icons/BackPurpleIcon.jsx
+++ src/app/icons/BackPurpleIcon.jsx
@@ -4,7 +4,7 @@ export const BackPurpleIcon = ({onClick}) => {
     return (
         <div className={'back-purple-icon'} onClick={onClick}>
             <svg width="14" height="14" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path id="Vector 39" d="M10.0769 1L2 9M2 9L10.0769 17M2 9H17" stroke="#9746FF" strokeWidth="2"
+                <path id="Vector 39" d="M10.0769 1L2 9M2 9L10.0769 17M2 9H17" stroke="#8540DD" strokeWidth="2"
                       strokeLinecap="round"/>
             </svg>
         </div>
