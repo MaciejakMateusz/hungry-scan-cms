003ea44b16763941d782733b30f78617605544ce
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-11-15 22:19:30 +0100

    Updated ContentSizeIndicator.style.js

--- src/app/hungryscan/cms/shared-components/ContentSizeIndicator.style.js
+++ src/app/hungryscan/cms/shared-components/ContentSizeIndicator.style.js
@@ -3,6 +3,6 @@ import styled from "styled-components";
 export const IndicatorContainer = styled.span`
     padding: 2px 9px;
     border-radius: 8px;
-    background: #E0F3FF;
-    color: #016DFF;
+    background: #F5EDFF;
+    color: #8540DD;
 `
