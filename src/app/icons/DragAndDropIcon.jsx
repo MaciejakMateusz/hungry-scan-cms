78d268381a35c17abac6cd214a06d759eaaf638a
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-11-15 22:19:56 +0100

    Updated DragAndDropIcon.jsx

--- src/app/icons/DragAndDropIcon.jsx
+++ src/app/icons/DragAndDropIcon.jsx
@@ -1,5 +1,5 @@
 export const DragAndDropIcon = ({disabled}) => {
-    const fill = disabled ? '#707070' : '#9746FF';
+    const fill = disabled ? '#707070' : '#8540DD';
     return (
         <svg width="11" height="18" viewBox="0 0 11 18" fill="none" xmlns="http://www.w3.org/2000/svg">
             <circle cx="1.5" cy="2" r="1.5" fill={fill}/>
