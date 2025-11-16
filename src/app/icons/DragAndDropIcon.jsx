e0cd7babcfa2ae631951ee9a4ac6f63382aa1f2c
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-11-16 15:48:59 +0100

    Updated DragAndDropIcon.jsx

--- src/app/icons/DragAndDropIcon.jsx
+++ src/app/icons/DragAndDropIcon.jsx
@@ -1,13 +1,13 @@
 export const DragAndDropIcon = ({disabled}) => {
     const fill = disabled ? '#707070' : '#8540DD';
     return (
-        <svg width="11" height="18" viewBox="0 0 11 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="1.5" cy="2" r="1.5" fill={fill}/>
-            <circle cx="8.5" cy="2" r="1.5" fill={fill}/>
-            <circle cx="1.5" cy="9" r="1.5" fill={fill}/>
-            <circle cx="8.5" cy="9" r="1.5" fill={fill}/>
-            <circle cx="1.5" cy="16" r="1.5" fill={fill}/>
-            <circle cx="8.5" cy="16" r="1.5" fill={fill}/>
+        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="7" cy="3.25" r="1.5" fill={fill}/>
+            <circle cx="7" cy="10" r="1.5" fill={fill}/>
+            <circle cx="7" cy="16.75" r="1.5" fill={fill}/>
+            <circle cx="13" cy="3.25" r="1.5" fill={fill}/>
+            <circle cx="13" cy="10" r="1.5" fill={fill}/>
+            <circle cx="13" cy="16.75" r="1.5" fill={fill}/>
         </svg>
     );
 }
