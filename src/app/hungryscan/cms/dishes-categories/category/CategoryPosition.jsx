fadad1a52a2d1b08f039143588060bb60e00d84e
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-11-16 15:46:34 +0100

    Updated CategoryPosition.jsx

--- src/app/hungryscan/cms/dishes-categories/category/CategoryPosition.jsx
+++ src/app/hungryscan/cms/dishes-categories/category/CategoryPosition.jsx
@@ -1,5 +1,9 @@
 import React, {useRef, useState} from "react";
-import {setCategory, setNewDishFormActive} from "../../../../../slices/dishesCategoriesSlice";
+import {
+    setCategory,
+    setEditCategoryFormActive,
+    setNewDishFormActive
+} from "../../../../../slices/dishesCategoriesSlice";
 import {useDispatch, useSelector} from "react-redux";
 import {useTranslation} from "react-i18next";
 import {ContentSizeIndicator} from "../../shared-components/ContentSizeIndicator";
@@ -7,13 +11,14 @@ import {RecordOptionsButton} from "../../shared-components/RecordOptionsButton";
 import {useCategoryContextPositions} from "../../../../../hooks/useCategoryContextPositions";
 import {useOutsideClick} from "../../../../../hooks/useOutsideClick";
 
-export const CategoryPosition = ({category}) => {
+export const CategoryPosition = ({category, expandHandler, expanded}) => {
     const {t} = useTranslation();
     const dispatch = useDispatch();
     const contextRef = useRef();
     const {menu} = useSelector(state => state.cms.fetchActiveMenu);
     const {restaurant} = useSelector(state => state.dashboard.view);
     const restaurantLanguage = restaurant?.value.settings.language.toLowerCase();
+    const [hovered, setHovered] = useState(false);
     const [contextWindowActive, setContextWindowActive] = useState(false);
     const [contextDetailsWindowActive, setContextDetailsWindowActive] = useState(false);
     const categoryContextPositions = useCategoryContextPositions({
@@ -25,26 +30,41 @@ export const CategoryPosition = ({category}) => {
         setContextWindowActive(false);
     }, contextWindowActive);
 
+    const handleEdit = () => {
+        dispatch(setCategory(category));
+        dispatch(setEditCategoryFormActive(true));
+    };
+
+    const handleStopPropagation = e => {
+        e.stopPropagation();
+    }
+
     if (!menu || !menu.categories) return null;
 
     return (
-        <div className={'category-container-header'}>
-            <div className={'category-info'}>
+        <div className={'category-container-header'}
+             onMouseEnter={() => setHovered(true)}
+             onMouseLeave={() => setHovered(false)}>
+            <div className={'category-info'} onClick={handleEdit}>
                 <span className={'text-ellipsis'} style={{maxWidth: '50vw'}}>
                     {category.name[restaurantLanguage]}
                 </span>
                 <ContentSizeIndicator size={category.menuItems.length}/>
-                <RecordOptionsButton className={'record-context-actions-button'}
-                                     onClick={() => setContextWindowActive(!contextWindowActive)}
-                                     contextWindowActive={contextWindowActive}
-                                     contextPositions={categoryContextPositions}
-                                     contextRef={contextRef}/>
+                <div onClick={handleStopPropagation}>
+                    <RecordOptionsButton className={'record-context-actions-button'}
+                                         onClick={() => setContextWindowActive(!contextWindowActive)}
+                                         dotsFill={hovered ? undefined : 'transparent'}
+                                         contextWindowActive={contextWindowActive}
+                                         contextPositions={categoryContextPositions}
+                                         contextRef={contextRef}/>
+                </div>
             </div>
+            <div className={'expandable-container'} onClick={() => expandHandler(!expanded)}/>
             <div className={'new-position-button'} onClick={() => {
                 dispatch(setCategory(category));
                 dispatch(setNewDishFormActive(true));
             }}>
-                + &nbsp;{t('newDish')}
+                + {t('newDish')}
             </div>
         </div>
     );
