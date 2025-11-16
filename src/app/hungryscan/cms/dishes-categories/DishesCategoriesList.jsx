db56f2e332db2f2c1ed4d5d6a84cfc5dbbb18bd9
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-11-16 15:47:36 +0100

    Updated DishesCategoriesList.jsx

--- src/app/hungryscan/cms/dishes-categories/DishesCategoriesList.jsx
+++ src/app/hungryscan/cms/dishes-categories/DishesCategoriesList.jsx
@@ -2,25 +2,20 @@ import React, {useEffect, useState} from "react";
 import {useTranslation} from "react-i18next";
 import {useDispatch, useSelector} from "react-redux";
 import {DecisionDialog} from "../dialog-windows/DecisionDialog";
-import {MenuItemPosition} from "./menu-item/MenuItemPosition";
-import {CategoryPosition} from "./category/CategoryPosition";
 import {FilteredMenuItems} from "./FilteredMenuItems";
 import {remove, setRemovalError} from "../../../../slices/objectRemovalSlice";
 import {
     setActiveRemovalType,
     setCategoryForAction,
-    setMenuItemForAction,
-    updateMenuItemsOrder
+    setMenuItemForAction
 } from "../../../../slices/dishesCategoriesSlice";
-import {DndContext} from '@dnd-kit/core';
-import {arrayMove, SortableContext} from '@dnd-kit/sortable';
 import {ReorderCategoriesDialog} from "../dialog-windows/ReorderCategoriesDialog";
 import {useConfirmationMessage} from "../../../../hooks/useConfirmationMessage";
 import {setCategoryRemoved} from "../../../../slices/categoryFormSlice";
 import {setMenuItemRemoved} from "../../../../slices/dishFormSlice";
 import {SwitchCategoryDialog} from "../dialog-windows/SwitchCategoryDialog";
 import {FormErrorDialog} from "../../../error/FormErrorDialog";
-import {DetailsDialog} from "../dialog-windows/DetailsDialog";
+import {CategoryContent} from "./CategoryContent";
 
 export const DishesCategoriesList = () => {
     const {t} = useTranslation();
@@ -85,51 +80,11 @@ export const DishesCategoriesList = () => {
                                isLoading={removalPending}/>
     };
 
-    const handleDragEnd = async (event, category) => {
-        const {active, over} = event;
-        if (!over || active.id === over.id) return;
-
-        const oldIndex = category.menuItems.findIndex(item => item.id.toString() === active.id);
-        const newIndex = category.menuItems.findIndex(item => item.id.toString() === over.id);
-
-        const newMenuItemsOrdered = arrayMove(category.menuItems, oldIndex, newIndex);
-        const newMenuItems = newMenuItemsOrdered.map((item, index) => ({
-            ...item,
-            displayOrder: index + 1,
-        }));
-
-        const newLocalCategories = localCategories.map(c => {
-            if (c.id === category.id) {
-                return {...c, menuItems: newMenuItems};
-            }
-            return c;
-        });
-        setLocalCategories(newLocalCategories);
-        await dispatch(updateMenuItemsOrder({menuItems: newMenuItems}));
-    };
-
     const renderCategories = () => {
-        return localCategories?.map(category => {
-            const menuItemIds = category.menuItems.map(item => item.id.toString());
-            return (
-                <div key={category.id} className={'category-container-new'}>
-                    <CategoryPosition category={category}/>
-                    {category.menuItems.length > 0 && <div className={'draggable-position-separator'}/>}
-                    <DndContext onDragEnd={(event) => handleDragEnd(event, category)}>
-                        <SortableContext items={menuItemIds}>
-                            {category.menuItems.map(menuItem => (
-                                <MenuItemPosition
-                                    key={`${menuItem.id}-${menuItem.displayOrder}`}
-                                    id={menuItem.id.toString()}
-                                    category={category}
-                                    menuItem={menuItem}
-                                />
-                            ))}
-                        </SortableContext>
-                    </DndContext>
-                </div>
-            );
-        });
+        return localCategories?.map(category => <CategoryContent key={category.id}
+                                                                 category={category}
+                                                                 localCategories={localCategories}
+                                                                 setLocalCategories={setLocalCategories}/>);
     };
 
     return (
