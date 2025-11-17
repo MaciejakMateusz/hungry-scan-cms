763f8e1764ce87c381c2933c50c92c39d313ee56
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-11-17 21:16:53 +0100

    Updated SearchIcon.jsx

--- src/app/icons/SearchIcon.jsx
+++ src/app/icons/SearchIcon.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 
-export const SearchIcon = () => {
+export const SearchIcon = ({color = "#222"}) => {
     return (
-        <svg width="15" height="15" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <ellipse cx="6.01865" cy="6.01866" rx="6.01865" ry="6.01866" fill="#6940C6"/>
-            <ellipse cx="6.01881" cy="6.01857" rx="4.16676" ry="4.16676" fill="#FFF"/>
-            <rect width="1.85189" height="8.99017" rx="0.925946"
-                  transform="matrix(0.707106 -0.707107 0.707106 0.707107 8.33374 9.64294)" fill="#6940C6"/>
+        <svg width="18" height="18" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M13.1936 13.7287C14.5976 12.6305 15.5 10.9206 15.5 9C15.5 5.68629 12.8137 3 9.5 3C6.18629 3 3.5 5.68629 3.5 9C3.5 12.3137 6.18629 15 9.5 15C10.8931 15 12.1753 14.5252 13.1936 13.7287ZM13.1936 13.7287L16.5 17"
+                  stroke={color} strokeWidth="1.25" strokeLinecap="round"/>
         </svg>
     );
 }
