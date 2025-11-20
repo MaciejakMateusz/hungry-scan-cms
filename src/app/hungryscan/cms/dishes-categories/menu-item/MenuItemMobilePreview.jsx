bf73106048ab773ea7346a624bdbddace016f2c6
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-11-20 07:05:59 +0100

    Updated MenuItemMobilePreview.jsx

--- src/app/hungryscan/cms/dishes-categories/menu-item/MenuItemMobilePreview.jsx
+++ src/app/hungryscan/cms/dishes-categories/menu-item/MenuItemMobilePreview.jsx
@@ -4,9 +4,6 @@ import {MenuItemDetails} from "./preview/MenuItemDetails";
 export const MenuItemMobilePreview = ({image}) => {
     return (
         <div className={'menu-item-mobile-preview-container'}>
-            <img className={'phone-frame'}
-                 src={`${process.env.PUBLIC_URL}/theme/images/phone-frame.png`}
-                 alt={'Phone frame'}/>
             <MenuItemDetails image={image}/>
         </div>
     );
