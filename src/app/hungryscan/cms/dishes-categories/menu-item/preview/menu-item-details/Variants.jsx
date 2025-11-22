c034a6e9f2f37f8f90d3ed3c90d5089573179f25
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-11-22 22:10:26 +0100

    Created new Variants

---
+++ src/app/hungryscan/cms/dishes-categories/menu-item/preview/menu-item-details/Variants.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import {useSelector} from "react-redux";
+import {useTranslation} from "react-i18next";
+import {MenuItemDetailsPosition} from "./MenuItemDetailsPosition.jsx";
+import {useGetTranslation} from "../../../../../../../hooks/useGetTranslation";
+import {formatPrice} from "../../../../../../../utils/utils";
+import {DefinitionText, PositionsWrapper} from "./Variants.style";
+
+export const Variants = () => {
+    const {t} = useTranslation();
+    const {variants, price} = useSelector(state => state.dishForm.form);
+    const preparedVariants = variants
+        ?.filter(v => v.available)
+        .sort((a, b) => a.displayOrder - b.displayOrder);
+    const getTranslation = useGetTranslation();
+
+    if (preparedVariants.length === 0) {
+        return null;
+    }
+
+    return (
+        <>
+            <DefinitionText>{t('variants')}:</DefinitionText>
+            <PositionsWrapper>
+                {preparedVariants.map(variant => (
+                    <MenuItemDetailsPosition name={getTranslation(variant.name)}
+                                             price={`${formatPrice(variant.price + Number(price))} zł`}
+                                             key={variant.id}
+                    />
+                ))}
+            </PositionsWrapper>
+        </>
+    );
+}
