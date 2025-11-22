05e15ef8e5cf0d527f223f0d720980fb4767a793
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-11-22 22:08:47 +0100

    Updated WelcomeTextContainer.jsx

--- src/app/hungryscan/cms/personalization/menu-preview/WelcomeTextContainer.jsx
+++ src/app/hungryscan/cms/personalization/menu-preview/WelcomeTextContainer.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import {useSelector} from "react-redux";
-import {getTranslation} from "../../../../../locales/langUtils";
+import {useGetTranslation} from "../../../../../hooks/useGetTranslation";
 
 export const WelcomeTextContainer = () => {
     const {menu} = useSelector(state => state.cms.fetchActiveMenu);
+    const getTranslation = useGetTranslation();
 
     if (!menu) return null;
 
