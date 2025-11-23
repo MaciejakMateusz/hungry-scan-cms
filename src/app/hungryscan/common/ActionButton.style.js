1eebcb1978e82d2ea0d769158693e94d389093b5
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-11-23 21:36:24 +0100

    Created ActionButton.style.js

---
+++ src/app/hungryscan/common/ActionButton.style.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+    border-radius: 16px;
+    background: #8540DD;
+    color: #FFF;
+    padding: 6px 12px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+    text-wrap: nowrap;
+    font-size: 13px;
+`;
