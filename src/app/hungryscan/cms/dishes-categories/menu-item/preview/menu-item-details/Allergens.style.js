9c794ba955686eca0565ed8d5d5d5ef9bbed72c6
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-11-23 21:37:11 +0100

    Created Allergens.style.js

---
+++ src/app/hungryscan/cms/dishes-categories/menu-item/preview/menu-item-details/Allergens.style.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const TooltipHeader = styled.p`
+    margin: 0 0 8px 0;
+`
+
+export const Position = styled.div`
+    display: flex;
+    align-content: center;
+    gap: 6px;
+    margin-bottom: 6px;
+`
