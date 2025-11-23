c6b633cb633cf77470830c6c96c7ac86881457fa
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-11-23 21:36:44 +0100

    Created Additions.jsx

---
+++ src/app/hungryscan/cms/dishes-categories/menu-item/preview/menu-item-details/Additions.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import {useTranslation} from "react-i18next";
+import {useSelector} from "react-redux";
+import {MenuItemDetailsPosition} from "./MenuItemDetailsPosition.jsx";
+import {useGetTranslation} from "../../../../../../../hooks/useGetTranslation";
+import {formatPrice} from "../../../../../../../utils/utils";
+import {DefinitionText, OptionalText, PositionsWrapper} from "./Additions.style";
+
+export const Additions = () => {
+    const {t} = useTranslation();
+    const {chosenAdditions} = useSelector(state => state.dishAdditions.fetchIngredients);
+    const getTranslation = useGetTranslation();
+
+    if (chosenAdditions.length === 0) {
+        return null;
+    }
+
+    return (
+        <>
+            <DefinitionText>
+                {t('additions')}
+                <OptionalText> ({t('optional')}):</OptionalText>
+            </DefinitionText>
+            <PositionsWrapper>
+                {chosenAdditions
+                    ?.filter(addition => addition.value.available)
+                    .map(addition => (
+                    <MenuItemDetailsPosition name={getTranslation(addition.value.name)}
+                                             price={`+${formatPrice(addition.value.price)} zł`}
+                                             key={addition.value.id}
+                    />
+                ))}
+            </PositionsWrapper>
+        </>
+    );
+}
