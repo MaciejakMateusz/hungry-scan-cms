0334dc8c3054e187f69111c645419de4c538a22a
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-11-23 21:36:49 +0100

    Created Created Additions.jsx

---
+++ src/app/hungryscan/cms/dishes-categories/menu-item/preview/menu-item-details/Additions.style.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const DefinitionText = styled.div`
+    color: var(--Basic-900);
+    font-size: 14px;
+    font-weight: 400;
+    margin-bottom: 16px;
+`
+
+export const OptionalText = styled.span`
+    color: var(--Grey-700);
+    font-weight: 400;
+    font-size: 14px;
+`
+
+export const PositionsWrapper = styled.div`
+    margin-bottom: 35px;
+`
