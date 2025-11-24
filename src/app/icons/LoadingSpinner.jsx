6ffd9a9be5100781001d7d4fc822402ccf3c97d8
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-11-24 21:14:31 +0100

    Updated LoadingSpinner.jsx

--- src/app/icons/LoadingSpinner.jsx
+++ src/app/icons/LoadingSpinner.jsx
@@ -4,7 +4,7 @@ export const LoadingSpinner = ({buttonMode, customStyle}) => {
 
     return (
         <div className={buttonMode ? 'button-spinner-container' : 'spinner-container'}>
-            <div className={buttonMode ? 'button-spinner' : 'spinner'} style={customStyle ? customStyle : {}}/>
+            <div className={buttonMode ? 'loader-button' : 'loader'} style={customStyle ? customStyle : {}}/>
         </div>
     );
 };
