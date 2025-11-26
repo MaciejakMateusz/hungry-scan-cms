22a274adae30d88404e00e2392c3168069c7777a
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-11-26 20:05:00 +0100

    Updated useTranslatableTransformer.js

--- src/hooks/useTranslatableTransformer.js
+++ src/hooks/useTranslatableTransformer.js
@@ -1,10 +1,14 @@
-import {createTranslatable, updateTranslatable} from "../locales/langUtils";
+import {useSelector} from "react-redux";
 
 export const useTranslatableTransformer = ({obj, key}) => {
+    const {restaurant} = useSelector(state => state.dashboard.view);
+
     return value => {
+        const restaurantData = restaurant.value;
+        const language = restaurantData.settings.language.toLowerCase();
         if (!obj?.[key]) {
-            return createTranslatable(value);
+            return {id: null, [language]: value};
         }
-        return updateTranslatable(obj[key], value)
+        return {...obj[key], [language]: value};
     }
 }
