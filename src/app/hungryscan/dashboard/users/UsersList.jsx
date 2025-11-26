74a9c9be4ab1ab1dee0d3afb6182259a97449c8c
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-11-26 20:05:19 +0100

    Updated UsersList.jsx

--- src/app/hungryscan/dashboard/users/UsersList.jsx
+++ src/app/hungryscan/dashboard/users/UsersList.jsx
@@ -1,37 +1,18 @@
 import {useDispatch, useSelector} from "react-redux";
-import {
-    Action,
-    Actions,
-    TableData,
-    TableDataActions,
-    TableHead,
-    TableHeadCell,
-    TableHeadRow,
-    TableRow,
-    UsersTable
-} from "./UsersList.style";
-import {getTranslation} from "../../../../locales/langUtils";
+import {TableHead, TableHeadCell, TableHeadRow, UsersTable} from "./UsersList.style";
 import {useTranslation} from "react-i18next";
-import {EditIconNew} from "../../../icons/EditIconNew";
-import {DeleteIconNew} from "../../../icons/DeleteIconNew";
 import React from "react";
-import {
-    getUsers,
-    removeUser,
-    setEditUserFormActive, setRemovalError, setUserRemoved,
-    setUserToRemove,
-    setUserToUpdate
-} from "../../../../slices/usersSlice";
+import {getUsers, removeUser, setUserRemoved, setUserToRemove} from "../../../../slices/usersSlice";
 import {DecisionDialog} from "../../cms/dialog-windows/DecisionDialog";
-import {ErrorMessageDialog} from "../../../error/ErrorMessageDialog";
 import {useConfirmationMessage} from "../../../../hooks/useConfirmationMessage";
+import {UserPosition} from "./UserPosition";
 
-export const UsersList = () => {
+export const UsersList = ({executeFilter}) => {
     const {t} = useTranslation();
     const dispatch = useDispatch();
     const {users} = useSelector(state => state.users.getUsers);
-    const {userToRemove} = useSelector(state => state.users.view);
-    const {isLoading: userRemovalPending, removalError} = useSelector(state => state.users.removeUser);
+    const {userToRemove, filteringActive, filterValue} = useSelector(state => state.users.view);
+    const {isLoading: userRemovalPending} = useSelector(state => state.users.removeUser);
     const confirmUserRemoval = useConfirmationMessage(setUserRemoved);
 
     const handleUserRemoval = async e => {
@@ -39,51 +20,24 @@ export const UsersList = () => {
         const resultAction = await dispatch(removeUser());
         if (removeUser.fulfilled.match(resultAction)) {
             dispatch(setUserToRemove(null));
-            dispatch(getUsers());
+            filteringActive ? executeFilter(filterValue) : dispatch(getUsers());
             confirmUserRemoval();
         }
     };
 
-    const handleErrorDismiss = () => {
-        dispatch(setUserToRemove(null));
-        dispatch(setRemovalError(null));
-    }
-
     const UsersList = () => {
-        return users?.map((user) => (
-            <TableRow key={user.username}>
-                <TableData>{`${user.forename} ${user.surname}`}</TableData>
-                <TableData>{user.username}</TableData>
-                <TableData>{user.roles?.map((r) => getTranslation(r.displayedName)).join(', ')}</TableData>
-                <TableData>{user.restaurants?.map((r) => r.name).join(', ')}</TableData>
-                <TableData>{user.active ? t('active') : t('inactive')}</TableData>
-                <TableDataActions>
-                    <Actions>
-                        <Action onClick={() => {
-                            dispatch(setUserToUpdate(user));
-                            dispatch(setEditUserFormActive(true));
-                        }}>
-                            <EditIconNew/>
-                        </Action>
-                        <Action onClick={() => dispatch(setUserToRemove(user))}>
-                            <DeleteIconNew/>
-                        </Action>
-                    </Actions>
-                </TableDataActions>
-            </TableRow>
-        ));
+        return users?.map(user => <UserPosition key={user.username} user={user}/>);
     }
 
     return (
         <>
-            {(userToRemove && !removalError) &&
+            {userToRemove &&
                 <DecisionDialog msg={t('confirmUserRemoval')}
                                 objName={userToRemove.name}
                                 onSubmit={(e) => handleUserRemoval(e)}
                                 onCancel={() => dispatch(setUserToRemove(null))}
-                                isLoading={userRemovalPending}/>}
-            {removalError && <ErrorMessageDialog dismissHandler={() => handleErrorDismiss()}
-                                                 errorMessage={removalError.message}/>}
+                                isLoading={userRemovalPending}
+                                isRemoval={true}/>}
             <UsersTable>
                 <TableHead>
                     <TableHeadRow>
@@ -92,7 +46,7 @@ export const UsersList = () => {
                         <TableHeadCell>{t('roles')}</TableHeadCell>
                         <TableHeadCell>{t('restaurants')}</TableHeadCell>
                         <TableHeadCell>{t('status')}</TableHeadCell>
-                        <TableHeadCell>{t('actions')}</TableHeadCell>
+                        <TableHeadCell/>
                     </TableHeadRow>
                 </TableHead>
                 <tbody>
