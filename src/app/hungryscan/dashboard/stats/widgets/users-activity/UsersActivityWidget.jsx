398d7d351f3c1e9a4da43c36cb97da1707766eae
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-11-26 20:05:40 +0100

    Created UsersActivityWidget.jsx

---
+++ src/app/hungryscan/dashboard/stats/widgets/users-activity/UsersActivityWidget.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {UsersActivity} from "./UsersActivity";
+
+export const UsersActivityWidget = () => {
+    return (
+        <div className={'statistic-widget users'}>
+            <UsersActivity/>
+        </div>
+    );
+}
