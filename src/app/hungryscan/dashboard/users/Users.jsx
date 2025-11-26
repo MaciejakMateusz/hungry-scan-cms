7863cdb0be636adef82530d939ecf3ab34b1a1bb
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-11-26 20:05:46 +0100

    Updated Users.jsx

--- src/app/hungryscan/dashboard/users/Users.jsx
+++ src/app/hungryscan/dashboard/users/Users.jsx
@@ -1,51 +1,100 @@
 import React, {useEffect} from "react";
 import {useTranslation} from "react-i18next";
 import {useDispatch, useSelector} from "react-redux";
-import {getUsers, setNewUserFormActive} from "../../../../slices/usersSlice";
+import {
+    clearForm,
+    getUsers,
+    setFilterExpanded,
+    setFilteringActive,
+    setFilterValue,
+    setNewUserFormActive,
+    setRemovalError,
+    setUsers
+} from "../../../../slices/usersSlice";
 import {UsersList} from "./UsersList";
 import {SearchButton} from "../../cms/dishes-categories/SearchButton";
 import {NewUserForm} from "./form/NewUserForm";
 import {EditUserForm} from "./form/EditUserForm";
+import {FormErrorDialog} from "../../../error/FormErrorDialog";
+import {clearProfileForm} from "../../../../slices/userProfileSlice";
+import {filter} from "../../../../slices/filteringSlice";
+import {setIsInEditMode} from "../../../../slices/globalParamsSlice";
 
 export const Users = () => {
     const {t} = useTranslation();
     const dispatch = useDispatch();
-    const {newUserFormActive, editUserFormActive} = useSelector(state => state.users.view);
+    const {
+        newUserFormActive,
+        editUserFormActive,
+        filterExpanded,
+        filterValue
+    } = useSelector(state => state.users.view);
+    const {removalError} = useSelector(state => state.users.removeUser);
 
     useEffect(() => {
         dispatch(getUsers());
+        dispatch(clearForm());
+        dispatch(clearProfileForm());
     }, [dispatch]);
 
+    useEffect(() => {
+        if (!filterExpanded && filterValue !== '') {
+            dispatch(setFilterValue(''));
+            executeFilter('');
+        }
+    }, [dispatch, filterExpanded, filterValue]);
+
+    const handleSearchSubmit = async (e) => {
+        e.preventDefault()
+        dispatch(setFilterValue(e.target.value));
+        await executeFilter(e.target.value);
+    }
+
+    const executeFilter = async value => {
+        if ('' !== value) {
+            const resultAction = await dispatch(filter({path: 'admin/users', value: value}));
+            if (filter.fulfilled.match(resultAction)) {
+                dispatch(setUsers(resultAction.payload));
+            }
+        } else {
+            dispatch(getUsers());
+        }
+    }
+
     if (newUserFormActive) {
-        return (<NewUserForm/>);
+        return (<NewUserForm executeFilter={executeFilter}/>);
     } else if (editUserFormActive) {
-        return (<EditUserForm/>);
+        return (<EditUserForm executeFilter={executeFilter}/>);
     }
 
     return (
         <div className={'background'}>
+            <FormErrorDialog errorData={removalError} setErrorData={setRemovalError}/>
             <div className={'cms-padded-view-container'}>
                 <div className={'functions-header'}>
                     <div className={'section-heading'}>{t('users')}</div>
                     <div className={'flex-wrapper-gapped'}>
                         <div className={'general-button-new'}
-                             onClick={() => dispatch(setNewUserFormActive(true))}>
-                            + &nbsp;{t('addUser')}
+                             onClick={() => {
+                                 dispatch(setNewUserFormActive(true));
+                                 dispatch(setIsInEditMode(true));
+                             }}>
+                            + {t('addUser')}
                         </div>
-                        <SearchButton filterExpanded={false}
-                                      onExpand={() => {
-                                      }}
-                                      filterValue={''}
-                                      onSubmit={() => {
-                                      }}
+                        <SearchButton filterExpanded={filterExpanded}
+                                      onExpand={() => dispatch(setFilterExpanded(!filterExpanded))}
+                                      filterValue={filterValue}
+                                      onSubmit={handleSearchSubmit}
                                       onClear={() => {
+                                          dispatch(setFilterValue(''));
+                                          dispatch(getUsers());
                                       }}
                         />
                     </div>
                 </div>
                 <div className={'form-grid'}>
-                    <div className={'padded-form-fragment'}>
-                        <UsersList/>
+                    <div className={'padded-form-fragment users'}>
+                        <UsersList executeFilter={executeFilter}/>
                     </div>
                 </div>
             </div>
