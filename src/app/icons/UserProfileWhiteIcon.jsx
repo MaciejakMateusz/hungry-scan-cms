720ef123e8d7b55e780afed2973af7dfd91b4ad1
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-11-26 20:05:49 +0100

    Updated UserProfileWhiteIcon.jsx

--- src/app/icons/UserProfileWhiteIcon.jsx
+++ src/app/icons/UserProfileWhiteIcon.jsx
@@ -1,19 +1,14 @@
 import React from "react";
-import {setCurrentView} from "../../slices/globalParamsSlice";
 import {USER_PROFILE} from "../../utils/viewsConstants";
-import {useDispatch} from "react-redux";
+import {useSwitchView} from "../../hooks/useSwitchView";
 
-export const UserProfileWhiteIcon = () => {
-    const dispatch = useDispatch();
-
+export const UserProfileWhiteIcon = ({clearStateHandler}) => {
+    const handleSwitchView = useSwitchView({clearStateHandler: clearStateHandler});
     return (
-        <div className={'profile-icon-wrapper'} onClick={() => dispatch(setCurrentView(USER_PROFILE))}>
-            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <g id="Group">
-                    <path id="Vector"
-                          d="M2.43442 15.4936H13.3213C14.6351 15.4936 15.2557 15.1011 15.2557 14.2526C15.2557 12.1188 12.4498 9.05426 7.87456 9.05426C3.3059 9.05426 0.5 12.1188 0.5 14.2526C0.5 15.1011 1.1206 15.4936 2.43442 15.4936ZM2.11091 14.4362C1.78741 14.4362 1.66858 14.3539 1.66858 14.126C1.66858 12.6507 3.90669 10.118 7.87456 10.118C11.849 10.118 14.0872 12.6507 14.0872 14.126C14.0872 14.3539 13.9683 14.4362 13.6448 14.4362H2.11091ZM7.88774 7.97783C9.88819 7.97783 11.4991 6.28724 11.4991 4.19143C11.4991 2.1336 9.88819 0.5 7.88774 0.5C5.89393 0.5 4.2698 2.15892 4.2698 4.20409C4.2698 6.29358 5.88732 7.97783 7.88774 7.97783ZM7.88774 6.92042C6.54753 6.92042 5.43838 5.73005 5.43838 4.20409C5.43838 2.72245 6.54094 1.55741 7.88774 1.55741C9.23458 1.55741 10.3305 2.70346 10.3305 4.19143C10.3305 5.71739 9.22802 6.92042 7.88774 6.92042Z"
-                          fill="#FAFAFA"/>
-                </g>
+        <div className={'profile-icon-wrapper'} onClick={() => handleSwitchView(USER_PROFILE)}>
+            <svg width="18" height="18" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M10 2C12.7614 2 15 4.23858 15 7C15 8.68525 14.1652 10.1743 12.8877 11.0801C15.2255 11.4991 17 13.5416 17 16L16.9893 16.2041C16.8938 17.1457 16.1457 17.8938 15.2041 17.9893L15 18H5L4.7959 17.9893C3.85435 17.8938 3.1062 17.1457 3.01074 16.2041L3 16C3 13.5419 4.77397 11.4995 7.11133 11.0801C5.83419 10.1742 5 8.68496 5 7C5 4.23858 7.23858 2 10 2ZM8 12.25C5.92893 12.25 4.25 13.9289 4.25 16C4.25 16.4142 4.58579 16.75 5 16.75H15C15.4142 16.75 15.75 16.4142 15.75 16C15.75 13.9289 14.0711 12.25 12 12.25H8ZM10 3.25C7.92893 3.25 6.25 4.92893 6.25 7C6.25 9.07107 7.92893 10.75 10 10.75C12.0711 10.75 13.75 9.07107 13.75 7C13.75 4.92893 12.0711 3.25 10 3.25Z"
+                      fill="#FAFAFA"/>
             </svg>
         </div>
     );
