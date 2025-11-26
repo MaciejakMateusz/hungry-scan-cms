90045b7f6ee389fe00ca695d87d4411afd032f93
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-11-26 20:04:08 +0100

    Updated Widgets.jsx

--- src/app/hungryscan/dashboard/stats/widgets/Widgets.jsx
+++ src/app/hungryscan/dashboard/stats/widgets/Widgets.jsx
@@ -3,17 +3,21 @@ import {QrWidget} from "./qr/QrWidget";
 import {UsersActivityWidget} from "./users-activity/UsersActivityWidget";
 import {QrFrequencyWidget} from "./qr-freq/QrFrequencyWidget";
 import {PopularDishesWidget} from "./popular-dishes/PopularDishesWidget";
+import {useWindowWidth} from "../../../../../hooks/useWindowWidth";
 
 export const Widgets = () => {
+    const width = useWindowWidth();
+    const isWide = width > 1300;
+
     return (
         <div className={'statistics-widgets-layout'}>
             <div className={'widgets-left-col'}>
                 <QrWidget/>
-                <UsersActivityWidget/>
+                {isWide ? <UsersActivityWidget/> : <QrFrequencyWidget/>}
             </div>
             <div className={'widgets-right-col'}>
-                <QrFrequencyWidget/>
-                <PopularDishesWidget/>
+                {isWide ? <QrFrequencyWidget/> : <PopularDishesWidget/>}
+                {isWide ? <PopularDishesWidget/> : <UsersActivityWidget/>}
             </div>
         </div>
     );
