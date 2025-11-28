e4e9af19db725f76f798ee794dcd79542d2a2083
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-11-28 21:44:31 +0100

    Updated BorderedButton.jsx

--- src/app/hungryscan/common/BorderedButton.jsx
+++ src/app/hungryscan/common/BorderedButton.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import {Button} from "./BorderedButton.style";
 
-export const BorderedButton = ({onClick, icon, text, isBordered, style}) => {
+export const BorderedButton = ({onClick, icon, text, isBordered, isMobile, style}) => {
     return (
-        <Button onClick={onClick} style={{...style, border: isBordered ? '1px solid #EDEFF3' : 'none'}}>
+        <Button onClick={onClick}
+                style={{
+                    ...style,
+                    border: isBordered ? '1px solid #EDEFF3' : 'none',
+                    padding: isMobile ? '6px 6px' : '6px 12px'
+        }}>
             {icon && <span>{icon}</span>}
-            {text && <span>{text}</span>}
+            {(text && !isMobile) && <span>{text}</span>}
         </Button>
     );
 }
