8c665e203451d8fa9ab620ba1d53abd598016e7a
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-11-30 17:13:50 +0100

    Updated BorderedButton.jsx

--- src/app/hungryscan/common/BorderedButton.jsx
+++ src/app/hungryscan/common/BorderedButton.jsx
@@ -1,16 +1,30 @@
 import React from "react";
 import {Button} from "./BorderedButton.style";
+import {LoadingSpinner} from "../../icons/LoadingSpinner";
 
-export const BorderedButton = ({onClick, icon, text, isBordered, isMobile, style}) => {
+export const BorderedButton = ({onClick, icon, text, isBordered, isMobile, style, isLoading}) => {
+
+    const renderContent = () => {
+        if (isLoading) {
+            return (<LoadingSpinner buttonMode={true}/>);
+        }
+
+        return (
+            <>
+                {icon && <span>{icon}</span>}
+                {(text && !isMobile) && <span>{text}</span>}
+            </>
+        );
+    }
+
     return (
         <Button onClick={onClick}
                 style={{
                     ...style,
                     border: isBordered ? '1px solid #EDEFF3' : 'none',
                     padding: isMobile ? '6px 6px' : style?.padding
-        }}>
-            {icon && <span>{icon}</span>}
-            {(text && !isMobile) && <span>{text}</span>}
+                }}>
+            {renderContent()}
         </Button>
     );
 }
