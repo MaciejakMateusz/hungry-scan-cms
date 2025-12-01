b7c8f39f108829236be5e85b641c91f7b19e3789
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-12-01 19:08:24 +0100

    Updated PopularDishesWidget.jsx

--- src/app/hungryscan/dashboard/stats/widgets/popular-dishes/PopularDishesWidget.jsx
+++ src/app/hungryscan/dashboard/stats/widgets/popular-dishes/PopularDishesWidget.jsx
@@ -1,68 +1,15 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import {TopDishesChart} from "./TopDishesChart";
-import {useDispatch, useSelector} from "react-redux";
-import {getPopularItemsStats, setChosenMenu} from "../../../../../../slices/statisticsSlice";
-import Select from "react-select";
-import {chartStyles} from "../../../../../../selectStyles";
-import {CustomNoOptionsMessage} from "../../../../cms/form-components/CustomNoOptionsMessage";
-import {useTranslation} from "react-i18next";
+import {PopularDishesMenuSelector} from "./PopularDishesMenuSelector";
+import {useWindowWidth} from "../../../../../../hooks/useWindowWidth";
 
 export const PopularDishesWidget = () => {
-    const {t} = useTranslation();
-    const dispatch = useDispatch();
-    const {restaurant} = useSelector(state => state.dashboard.view);
-    const {
-        period,
-        chosenYear,
-        chosenMonth,
-        chosenWeek,
-        chosenDay,
-        chosenMenu
-    } = useSelector(state => state.statistics.view);
-    const [menus, setMenus] = useState([]);
-
-    useEffect(() => {
-        if (!period) return;
-        if (period === "year" && !chosenYear) return;
-        if (period === "month" && (!chosenYear || !chosenMonth)) return;
-        if (period === "week" && (!chosenYear || !chosenWeek)) return;
-        if (period === "day" && !chosenDay) return;
-        if (!chosenMenu) return;
-        const params = {
-            year: chosenYear,
-            month: chosenMonth,
-            week: chosenWeek,
-            day: chosenDay,
-            menu: chosenMenu
-        };
-        dispatch(getPopularItemsStats({period, params}));
-    }, [chosenDay, chosenMenu, chosenMonth, chosenWeek, chosenYear, dispatch, period]);
-
-    useEffect(() => {
-        const menus = restaurant?.value.menus;
-        const mappedMenus = menus?.map(menu => ({
-            value: menu.id,
-            label: menu.name,
-        }));
-        setMenus(mappedMenus);
-        if (mappedMenus?.length > 0) {
-            dispatch(setChosenMenu(mappedMenus[0]));
-        }
-    }, [dispatch, restaurant?.value.menus]);
+    const windowWidth = useWindowWidth();
+    const isWide = windowWidth > 730;
 
     return (
         <div className={'statistic-widget popular-dishes'}>
-            <div className={'widget-mode-selector'} style={{top: '40px'}}>
-                <Select id={'menu-popular-dishes-selector'}
-                        name={'menu-popular-dishes-selector'}
-                        value={chosenMenu}
-                        placeholder={t('choose')}
-                        options={menus}
-                        onChange={(selected) => dispatch(setChosenMenu(selected))}
-                        styles={chartStyles}
-                        components={{NoOptionsMessage: CustomNoOptionsMessage}}
-                        isSearchable={false}/>
-            </div>
+            {isWide && <PopularDishesMenuSelector/>}
             <TopDishesChart/>
         </div>
     );
