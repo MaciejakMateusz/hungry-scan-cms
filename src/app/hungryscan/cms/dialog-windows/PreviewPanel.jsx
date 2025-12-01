300f9c7347338981079aabdf9fe04baa3a46a67d
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-12-01 19:08:46 +0100

    Updated PreviewPanel

--- src/app/hungryscan/cms/dialog-windows/PreviewPanel.jsx
+++ src/app/hungryscan/cms/dialog-windows/PreviewPanel.jsx
@@ -1,19 +1,12 @@
 import React from "react";
-import {CloseButton, Header, SidePanel} from "./PreviewPanel.style";
-import {useDispatch, useSelector} from "react-redux";
-import {setPreviewActive} from "../../../../slices/globalParamsSlice";
-import {useTranslation} from "react-i18next";
+import {SidePanel} from "./PreviewPanel.style";
+import {useSelector} from "react-redux";
 
 export const PreviewPanel = ({content}) => {
-    const {t} = useTranslation();
-    const dispatch = useDispatch();
     const {previewActive} = useSelector((state) => state.globalParams.globalParams);
 
     return (
         <SidePanel $previewActive={previewActive}>
-            <Header>
-                <CloseButton onClick={() => dispatch(setPreviewActive(false))}>{t('close')} ></CloseButton>
-            </Header>
             {previewActive && content}
         </SidePanel>
     );
--- src/app/hungryscan/cms/dialog-windows/PreviewPanel.style.js
+++ src/app/hungryscan/cms/dialog-windows/PreviewPanel.style.js
@@ -1,32 +1,8 @@
-import styled, { keyframes } from "styled-components";
-
-const slideIn = keyframes`
-    from {
-        transform: translateX(100%);
-    }
-    to {
-        transform: translateX(0);
-    }
-`;
+import styled from "styled-components";
 
 export const SidePanel = styled.div`
-    display: ${({ $previewActive }) => $previewActive};
-    width: 500px;
-    height: 100%;
+    display: ${({ $previewActive }) => $previewActive ? 'block' : 'none'};
+    width: 310px;
+    height: 94%;
     background: #FFF;
-    z-index: 9999;
-
-    animation: ${slideIn} 0.5s ease-in-out forwards;
-`;
-
-export const Header = styled.div`
-    height: 30px;
-    background: #F6F6F6;
-    display: flex;
-    padding: 10px;
-    box-sizing: border-box;
-`;
-export const CloseButton = styled.span`
-    font-size: 1rem;
-    cursor: pointer;
 `;
