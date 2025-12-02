92a4f0864e0b6dee42dc0bd5cb89da2658aed91a
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-12-02 18:20:09 +0100

    Updated ImageSection.jsx

--- src/app/hungryscan/cms/dishes-categories/menu-item/preview/menu-item-details/ImageSection.jsx
+++ src/app/hungryscan/cms/dishes-categories/menu-item/preview/menu-item-details/ImageSection.jsx
@@ -1,6 +1,5 @@
 import React, {useEffect, useMemo} from "react";
-import {ImageContainer, ReturnButton} from "./ImageSection.style";
-import {ReactSVG} from "react-svg";
+import {ImageContainer} from "./ImageSection.style";
 
 export const ImageSection = ({image}) => {
     const objectUrl = useMemo(() => image && URL.createObjectURL(image), [image]);
@@ -25,9 +24,6 @@ export const ImageSection = ({image}) => {
 
     return (
         <section>
-            <ReturnButton>
-                <ReactSVG src={`${process.env.PUBLIC_URL}/theme/icons/chevron-left.svg`}/>
-            </ReturnButton>
             {renderImage()}
         </section>
     );
