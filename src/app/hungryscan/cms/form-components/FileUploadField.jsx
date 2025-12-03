1ac54858072032a3ea0997a7b1c01b3112a95cd6
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-12-03 20:39:32 +0100

    Updated FileUploadField.jsx

--- src/app/hungryscan/cms/form-components/FileUploadField.jsx
+++ src/app/hungryscan/cms/form-components/FileUploadField.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import {useTranslation} from "react-i18next";
 import {useDispatch, useSelector} from "react-redux";
 import {setHasImage, setIsImageCleared} from "../../../../slices/dishFormSlice";
@@ -7,13 +7,14 @@ import {s3BucketUrl} from "../../../../apiData";
 import {InteractiveMenuItemImage} from "../dishes-categories/menu-item/InteractiveMenuItemImage";
 import {CloseIcon} from "../../../icons/CloseIcon";
 
-export const FileUploadField = ({setFile, file}) => {
+export const FileUploadField = React.memo(({file, setFile}) => {
     const {t} = useTranslation();
     const dispatch = useDispatch();
     const {id, updated, hasImage, isImageCleared} = useSelector(state => state.dishForm.form);
+    const [previewUrl, setPreviewUrl] = useState(null);
 
     useEffect(() => {
-        if (!hasImage) return;
+        if (!hasImage || file || isImageCleared || !id) return;
 
         async function urlToFile(url, filename = "menu-item-image.jpg") {
             const response = await fetch(url, {mode: "cors", cache: "no-cache"});
@@ -22,58 +23,100 @@ export const FileUploadField = ({setFile, file}) => {
         }
 
         (async () => {
-            const file = await urlToFile(`${s3BucketUrl}/menuItems/${id}.png?t=${updated}`);
-            setFile(file);
+            const remoteUrl = `${s3BucketUrl}/menuItems/${id}.png?t=${updated}`;
+            const fetchedFile = await urlToFile(remoteUrl);
+            setFile(fetchedFile);
+
             dispatch(setHasImage(true));
         })();
-    }, [dispatch, hasImage, id, setFile, updated]);
+    }, [dispatch, hasImage, id, updated, file, isImageCleared, setFile]);
+
+    useEffect(() => {
+        if (!file) {
+            setPreviewUrl(null);
+            return;
+        }
+
+        const objectUrl = URL.createObjectURL(file);
+        setPreviewUrl(objectUrl);
+
+        return () => {
+            URL.revokeObjectURL(objectUrl);
+        };
+    }, [file]);
 
     const handleFileChange = (e) => {
-        const file = e.target.files[0];
-        setFile(file);
+        const newFile = e.target.files[0];
+        if (!newFile) return;
+        setFile(newFile);
         dispatch(setIsImageCleared(false));
+        dispatch(setHasImage(true));
     };
 
     const clearFileData = () => {
         setFile(null);
+        setPreviewUrl(null);
         dispatch(setHasImage(false));
         dispatch(setIsImageCleared(true));
-    }
+    };
 
     const renderImage = () => {
-        let fileSource = null;
-        if (file) fileSource = URL.createObjectURL(file);
-        if (hasImage && !file) fileSource = `${s3BucketUrl}/menuItems/${id}.png?t=${updated}`;
-        if (!fileSource || isImageCleared) return;
-        return (<InteractiveMenuItemImage src={fileSource} hasImage={hasImage}/>);
-    }
+        if (isImageCleared) return null;
+
+        if (file && previewUrl) {
+            return (
+                <InteractiveMenuItemImage
+                    src={previewUrl}
+                    hasImage={true}
+                />
+            );
+        }
+
+        if (!file && hasImage && id) {
+            const remoteUrl = `${s3BucketUrl}/menuItems/${id}.png?t=${updated}`;
+            return (
+                <InteractiveMenuItemImage
+                    src={remoteUrl}
+                    hasImage={hasImage}
+                />
+            );
+        }
+
+        return null;
+    };
 
     const renderButton = () => {
-        if (isImageCleared || (!hasImage && !file)) {
+        const hasAnyImage = (!isImageCleared && (hasImage || file));
+
+        if (!hasAnyImage) {
             return (
                 <>
                     <label htmlFor={'dish-image'}
                            className={'custom-file-upload label'}>
                         {t('chooseFile')}
                     </label>
-                    <input type={'file'}
-                           id={'dish-image'}
-                           name={'imageName'}
-                           onChange={(e) => handleFileChange(e)}
-                           accept={'.png'}
-                           className={'file-input'}
+                    <input
+                        type={'file'}
+                        id={'dish-image'}
+                        name={'imageName'}
+                        onChange={handleFileChange}
+                        accept={'.png'}
+                        className={'file-input'}
                     />
                 </>
             );
         }
+
         return (
-            <div className={'custom-file-upload label clear'}
-                 onClick={clearFileData}>
+            <div
+                className={'custom-file-upload label clear'}
+                onClick={clearFileData}
+            >
                 <span>{t('clear')}</span>
                 <CloseIcon/>
             </div>
         );
-    }
+    };
 
     return (
         <div className={'form-field-wrapper'}>
@@ -91,4 +134,4 @@ export const FileUploadField = ({setFile, file}) => {
             </div>
         </div>
     );
-}
+});
