74a9102409cead9c3b2491cf662c21707744a15b
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-12-03 20:40:06 +0100

    Updated PreviewPanel.jsx

--- src/app/hungryscan/cms/dialog-windows/PreviewPanel.jsx
+++ src/app/hungryscan/cms/dialog-windows/PreviewPanel.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import {SidePanel} from "./PreviewPanel.style";
 import {useSelector} from "react-redux";
+import {ClosePreviewButton} from "./ClosePreviewButton";
+import {useWindowWidth} from "../../../../hooks/useWindowWidth";
 
-export const PreviewPanel = ({content}) => {
+export const PreviewPanel = ({content, personalizationMode}) => {
     const {previewActive} = useSelector((state) => state.globalParams.globalParams);
+    const windowWidth = useWindowWidth();
+    const isMobile = windowWidth <= 1000;
 
     return (
-        <SidePanel $previewActive={previewActive}>
+        <SidePanel $previewActive={previewActive} $personalizationMode={personalizationMode}>
             {previewActive && content}
+            {isMobile && <ClosePreviewButton/>}
         </SidePanel>
     );
 }
