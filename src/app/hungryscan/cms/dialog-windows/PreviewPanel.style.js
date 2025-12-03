b5a6c0ab0eb55e83de420b687331dc77c6899050
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-12-03 20:40:11 +0100

    Updated PreviewPanel.style.js

--- src/app/hungryscan/cms/dialog-windows/PreviewPanel.style.js
+++ src/app/hungryscan/cms/dialog-windows/PreviewPanel.style.js
@@ -3,6 +3,25 @@ import styled from "styled-components";
 export const SidePanel = styled.div`
     display: ${({ $previewActive }) => $previewActive ? 'block' : 'none'};
     width: 310px;
-    height: 94%;
+    height: ${({ $personalizationMode }) => $personalizationMode ? '100%' : '94%'};
     background: #FFF;
+
+    @media (max-width: 1000px) {
+        position: fixed;
+        right: 0;
+        top: 0;
+        height: 100%;
+        z-index: 9990;
+        width: ${({ $personalizationMode }) => $personalizationMode ? '350px' : '310px'};
+    }
+    
+    @media (max-width: 550px) {
+        position: fixed;
+        right: 0;
+        top: 0;
+        height: 100%;
+        z-index: 9990;
+        max-width: unset;
+        width: 100%;
+    }
 `;
