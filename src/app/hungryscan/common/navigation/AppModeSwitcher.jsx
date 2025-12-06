8a06cd62035ee194044a856289ce6b7d2d26ecdd
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-12-06 22:29:44 +0100

    Updated AppModeSwitcher.jsx

--- src/app/hungryscan/common/navigation/AppModeSwitcher.jsx
+++ src/app/hungryscan/common/navigation/AppModeSwitcher.jsx
@@ -1,41 +1,27 @@
 import React from "react";
 import {useTranslation} from "react-i18next";
-import {setCmsActive, setCurrentView} from "../../../../slices/globalParamsSlice";
-import {DISHES_CATEGORIES, STATS} from "../../../../utils/viewsConstants";
-import {useDispatch, useSelector} from "react-redux";
-import {IndexedText, Switcher, SwitcherPill, SwitcherPillInactive, Wrapper} from "./AppModeSwitcher.style";
+import {useSelector} from "react-redux";
+import {IndexedText, Switcher, SwitcherPill, SwitcherPillInactive} from "./AppModeSwitcher.style";
 
-export const AppModeSwitcher = ({hasAccessToDashboard}) => {
+export const AppModeSwitcher = () => {
     const {t} = useTranslation();
-    const dispatch = useDispatch();
     const {cmsActive} = useSelector(state => state.globalParams.globalParams);
 
-    const switchAppMode = () => {
-        dispatch(setCmsActive(!cmsActive));
-        !cmsActive ?
-            dispatch(setCurrentView(DISHES_CATEGORIES)) :
-            dispatch(setCurrentView(STATS));
-    }
-
-    if (!hasAccessToDashboard) return;
-
     return (
-        <Wrapper>
-            <Switcher onClick={() => switchAppMode()}>
-                {cmsActive &&
-                    <SwitcherPillInactive $cmsActive={cmsActive}>
-                        <IndexedText>{t('dashboard')}</IndexedText>
-                    </SwitcherPillInactive>
-                }
-                <SwitcherPill $cmsActive={cmsActive}>
-                    <IndexedText>{cmsActive ? 'CMS' : t('dashboard')}</IndexedText>
-                </SwitcherPill>
-                {!cmsActive &&
-                    <SwitcherPillInactive $cmsActive={cmsActive}>
-                        <IndexedText>CMS</IndexedText>
-                    </SwitcherPillInactive>
-                }
-            </Switcher>
-        </Wrapper>
+        <Switcher>
+            {cmsActive &&
+                <SwitcherPillInactive $cmsActive={cmsActive}>
+                    <IndexedText>{t('dashboard')}</IndexedText>
+                </SwitcherPillInactive>
+            }
+            <SwitcherPill $cmsActive={cmsActive}>
+                <IndexedText>{cmsActive ? 'CMS' : t('dashboard')}</IndexedText>
+            </SwitcherPill>
+            {!cmsActive &&
+                <SwitcherPillInactive $cmsActive={cmsActive}>
+                    <IndexedText>CMS</IndexedText>
+                </SwitcherPillInactive>
+            }
+        </Switcher>
     );
 }
