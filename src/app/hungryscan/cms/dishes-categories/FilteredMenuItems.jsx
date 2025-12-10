03132fcf7108a3a7c6c29a8f3d5d48f4f2195577
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-12-10 20:12:58 +0100

    Updated FilteredMenuItems.jsx

--- src/app/hungryscan/cms/dishes-categories/FilteredMenuItems.jsx
+++ src/app/hungryscan/cms/dishes-categories/FilteredMenuItems.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import {useSelector} from "react-redux";
 import {MenuItemPosition} from "./menu-item/MenuItemPosition";
+import {useTranslation} from "react-i18next";
 
 export const FilteredMenuItems = () => {
+    const {t} = useTranslation();
     const {filteredItems} = useSelector(state => state.dishesCategories.view);
+
+    if (!filteredItems || filteredItems.length === 0) {
+        return <p className={'text-center'}>{t('noPositionsFound')}</p>
+    }
 
     return (
         <>
