b9984fc12d50354097bacfa36e8086eccbb2ecc7
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-12-11 17:20:41 +0100

    Updated PreviewPanel.jsx

--- src/app/hungryscan/cms/dialog-windows/PreviewPanel.jsx
+++ src/app/hungryscan/cms/dialog-windows/PreviewPanel.jsx
@@ -1,16 +1,26 @@
-import React from "react";
+import React, {useRef} from "react";
 import {SidePanel} from "./PreviewPanel.style";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {ClosePreviewButton} from "./ClosePreviewButton";
 import {useWindowWidth} from "../../../../hooks/useWindowWidth";
+import {useOutsideClick} from "../../../../hooks/useOutsideClick";
+import {setPreviewActive} from "../../../../slices/globalParamsSlice";
 
 export const PreviewPanel = ({content, personalizationMode}) => {
+    const dispatch = useDispatch();
     const {previewActive} = useSelector((state) => state.globalParams.globalParams);
     const windowWidth = useWindowWidth();
     const isMobile = windowWidth <= 1000;
+    const useContextRef = useRef(null);
+
+    useOutsideClick(useContextRef,
+        () => dispatch(setPreviewActive(false)),
+        previewActive);
 
     return (
-        <SidePanel $previewActive={previewActive} $personalizationMode={personalizationMode}>
+        <SidePanel $previewActive={previewActive}
+                   $personalizationMode={personalizationMode}
+                   ref={isMobile ? useContextRef : null}>
             {previewActive && content}
             {isMobile && <ClosePreviewButton/>}
         </SidePanel>
