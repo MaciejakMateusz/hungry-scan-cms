3e8d5092b61acfbbfbad2edb9b5e846202e8d855
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-12-11 17:20:53 +0100

    Updated RecordOptionsButton.jsx

--- src/app/hungryscan/cms/shared-components/RecordOptionsButton.jsx
+++ src/app/hungryscan/cms/shared-components/RecordOptionsButton.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useLayoutEffect, useRef, useState} from "react";
 import {ThreeDotsIcon} from "../../../icons/ThreeDotsIcon";
 import {ContextMenu} from "./ContextMenu";
 
@@ -8,22 +8,49 @@ export const RecordOptionsButton = ({
                                         contextWindowActive,
                                         contextPositions,
                                         contextRef,
-                                        windowPosition,
                                         className,
+                                        correctPosition = {y: 0, x: 140},
                                         dotsFill
                                     }) => {
+    const buttonRef = useRef(null);
+    const [coords, setCoords] = useState(null);
+
+    useLayoutEffect(() => {
+        if (contextWindowActive && buttonRef.current) {
+            const rect = buttonRef.current.getBoundingClientRect();
+
+            setCoords({
+                top: rect.bottom + window.scrollY - correctPosition.y,
+                left: rect.left + window.scrollX - correctPosition.x
+            });
+        } else if (!contextWindowActive) {
+            setCoords(null);
+        }
+    }, [contextWindowActive]);
+
+    const handleClick = (e) => {
+        onClick?.(e);
+    };
+
     return (
-        <div className={'relative-container'}>
-            <div className={className}
-                 style={style}
-                 tabIndex={-1}
-                 onClick={onClick}>
-                <ThreeDotsIcon fill={dotsFill}/>
+        <div className="relative-container">
+            <div
+                ref={buttonRef}
+                className={className}
+                style={style}
+                tabIndex={-1}
+                onClick={handleClick}
+            >
+                <ThreeDotsIcon fill={dotsFill} />
             </div>
-            {contextWindowActive &&
-                <ContextMenu positions={contextPositions}
-                             contextRef={contextRef}
-                             windowPosition={windowPosition}/>}
+
+            {contextWindowActive && (
+                <ContextMenu
+                    positions={contextPositions}
+                    contextRef={contextRef}
+                    coords={coords}
+                />
+            )}
         </div>
     );
 }
