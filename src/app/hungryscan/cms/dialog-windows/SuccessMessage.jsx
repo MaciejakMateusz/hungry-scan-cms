b881deade8667f17356e740b9b130363815eb2cd
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-12-12 16:31:54 +0100

    Updated SuccessMessage.jsx

--- src/app/hungryscan/cms/dialog-windows/SuccessMessage.jsx
+++ src/app/hungryscan/cms/dialog-windows/SuccessMessage.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import {AvailableIcon} from "../../../icons/AvailableIcon";
+import {CheckIcon} from "../../../icons/CheckIcon";
 
 export const SuccessMessage = ({text, onDismiss}) => {
     return (
         <div className={'success-msg'} onClick={onDismiss}>
-            <AvailableIcon width={'21'} height={'21'} fill={'#FFF'}/>
+            <CheckIcon width={'21'} height={'21'} fill={'#FFF'}/>
             {text}
         </div>
     );
