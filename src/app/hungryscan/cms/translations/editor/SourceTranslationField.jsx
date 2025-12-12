5b952a88b4c4cd891a580f00fb99df2b3ae52bb6
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-12-12 16:31:41 +0100

    Updated SourceTranslationField.jsx

--- src/app/hungryscan/cms/translations/editor/SourceTranslationField.jsx
+++ src/app/hungryscan/cms/translations/editor/SourceTranslationField.jsx
@@ -1,19 +1,27 @@
 import React from "react";
 import {useTranslation} from "react-i18next";
 import {useSelector} from "react-redux";
+import {LanguageArrowsIcon} from "../../../../icons/LanguageArrowsIcon";
 
 export const SourceTranslationField = ({value, type}) => {
     const {t} = useTranslation();
     const {restaurant} = useSelector(state => state.dashboard.view);
+    const {chosenDestinationLanguage} = useSelector(state => state.translations.view);
 
     return (
         <div className={'original-translation-box'}>
             <div className={'original-translation-header'}>
                 <span className={'translation-text-label'}>
-                    {t('originalText')} - {t(type)}
+                    {t(type)}
                 </span>
-                <div className={'language-label'}>
-                    {t(restaurant?.value.settings.language.toLowerCase())}
+                <div className={'language-labels-wrapper'}>
+                    <div className={'language-label'}>
+                        {t(restaurant?.value.settings.language.toLowerCase())}
+                    </div>
+                    <LanguageArrowsIcon/>
+                    <div className={'language-label'}>
+                        {t(chosenDestinationLanguage.value.toLowerCase())}
+                    </div>
                 </div>
             </div>
             <div className={'translation-value-content-container'}>
