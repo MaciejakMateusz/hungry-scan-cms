8158eeb0d8b7d41ad0c38ec6d5060264823cf985
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-12-15 21:59:33 +0100

    Updated Banner.style.js

--- src/app/hungryscan/cms/dishes-categories/menu-item/preview/menu-item-details/Banner.style.js
+++ src/app/hungryscan/cms/dishes-categories/menu-item/preview/menu-item-details/Banner.style.js
@@ -2,12 +2,12 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     display: flex;
-    padding: 3px 9px 3px 7px;
+    padding: 3px 10px 3px 10px;
     justify-content: center;
     align-items: center;
-    border-radius: 100px;
+    border-radius: 7px;
     background: var(--Basic-900);
     color: var(--Basic-200);
-    font-size: 10px;
-    font-weight: 600;
+    font-size: 13px;
+    font-weight: 400;
 `
