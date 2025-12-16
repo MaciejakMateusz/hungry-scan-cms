75ae067e3a6d79b590d3cc48914eda6573ca71b0
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-12-16 18:50:22 +0100

    Updated NavMenu.jsx

--- src/app/main/NavMenu.jsx
+++ src/app/main/NavMenu.jsx
@@ -9,32 +9,40 @@ export const NavMenu = () => {
     const {t} = useTranslation();
     const dispatch = useDispatch();
     const {userData} = useSelector(state => state.userProfile.getUserProfile);
+    const isBeta = process.env.REACT_APP_IS_BETA === 'true';
 
     useEffect(() => {
         dispatch(getUserProfile());
     }, [dispatch]);
 
+    const renderMenuButtons = () => {
+        if (isBeta) return;
+        return (
+            <div className={'main-page-menu-buttons'}>
+                <button className={'main-page-menu-button'} onClick={() => window.location.href = '/'}>
+                    Home
+                </button>
+                <button className={'main-page-menu-button'} onClick={() => window.location.href = '/our-offer'}>
+                    {t('ourOffer')}
+                </button>
+                <button className={'main-page-menu-button'} onClick={() => window.location.href = '/price-plans'}>
+                    {t('pricePlans')}
+                </button>
+                <button className={'main-page-menu-button'} onClick={() => window.location.href = '/about-us'}>
+                    {t('aboutUs')}
+                </button>
+                <button className={'main-page-menu-button'} onClick={() => window.location.href = '/contact'}>
+                    {t('contact')}
+                </button>
+            </div>
+        );
+    }
+
     return (
         <div className={'main-page-menu'}>
             <div className={'main-page-menu-wrapper'}>
                 <LogoGroup onClick={() => window.location.href = '/'}/>
-                <div className={'main-page-menu-buttons'}>
-                    <button className={'main-page-menu-button'} onClick={() => window.location.href = '/'}>
-                        Home
-                    </button>
-                    <button className={'main-page-menu-button'} onClick={() => window.location.href = '/our-offer'}>
-                        {t('ourOffer')}
-                    </button>
-                    <button className={'main-page-menu-button'} onClick={() => window.location.href = '/price-plans'}>
-                        {t('pricePlans')}
-                    </button>
-                    <button className={'main-page-menu-button'} onClick={() => window.location.href = '/about-us'}>
-                        {t('aboutUs')}
-                    </button>
-                    <button className={'main-page-menu-button'} onClick={() => window.location.href = '/contact'}>
-                        {t('contact')}
-                    </button>
-                </div>
+                {renderMenuButtons()}
                 <div className={'main-page-login-button-group'} onClick={() => window.location.href = '/sign-in'}>
                     <UserProfileIcon/>
                     <button className={'main-page-login-button'}>
