c6157d60aaacaf455319da4afa09a3072c412c40
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-12-17 21:37:52 +0100

    Updated Tooltip.jsx

--- src/app/hungryscan/cms/Tooltip.jsx
+++ src/app/hungryscan/cms/Tooltip.jsx
@@ -1,15 +1,16 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
-export const Tooltip = ({ children, content, topOffset = -40}) => {
+export const Tooltip = ({children, content, topOffset = -40, rightOffset = 0}) => {
     const [isVisible, setIsVisible] = useState(false);
     const [tooltipPosition, setTooltipPosition] = useState({});
 
     const handleMouseEnter = (event) => {
+
         const rect = event.currentTarget.getBoundingClientRect();
         setTooltipPosition({
             top: rect.top + topOffset,
-            left: rect.left + rect.width / 2,
+            left: (rect.left + rect.width / 2) + rightOffset,
         });
         setIsVisible(true);
     };
@@ -18,22 +19,25 @@ export const Tooltip = ({ children, content, topOffset = -40}) => {
         setIsVisible(false);
     };
 
+    const renderContent = () => {
+        if (content === '') return <></>;
+        return (
+            <div
+                className={'tooltip-content'}
+                style={{top: tooltipPosition.top, left: tooltipPosition.left}}>
+                {content}
+            </div>
+        );
+    }
+
     return (
         <>
-            <div
-                className={'tooltip-wrapper'}
-                onMouseEnter={handleMouseEnter}
-                onMouseLeave={handleMouseLeave}>
+            <div className={'tooltip-wrapper'}
+                 onMouseEnter={handleMouseEnter}
+                 onMouseLeave={handleMouseLeave}>
                 {children}
             </div>
-            {isVisible && ReactDOM.createPortal(
-                <div
-                    className={'tooltip-content'}
-                    style={{ top: tooltipPosition.top, left: tooltipPosition.left }}>
-                    {content}
-                </div>,
-                document.body
-            )}
+            {isVisible && ReactDOM.createPortal(renderContent(), document.body)}
         </>
     );
 };
