74a19095fc8f1268a1663b9e7a10b377aa9e85fb
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-12-18 17:57:54 +0100

    Updated TranslationRecord.style.js

--- src/app/hungryscan/cms/translations/TranslationRecord.style.js
+++ src/app/hungryscan/cms/translations/TranslationRecord.style.js
@@ -9,8 +9,16 @@ export const TranslationContainer = styled.div`
     padding-right: 20px;
     padding-left: 20px;
     position: relative;
+    min-width: 450px;
     
     &:hover {
         background: #F9F9F9;
     }
 `
+
+export const TextWrapper = styled.div`
+    grid-column: 3;
+    max-width: 340px;
+    width: 100%;
+    min-width: 300px;
+`;
