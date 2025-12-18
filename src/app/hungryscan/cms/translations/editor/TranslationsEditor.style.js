7a7266375afa4a2e6418186c87321eb008ed2eac
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-12-18 17:58:13 +0100

    Updated TranslationsEditor.style.js

--- src/app/hungryscan/cms/translations/editor/TranslationsEditor.style.js
+++ src/app/hungryscan/cms/translations/editor/TranslationsEditor.style.js
@@ -1,26 +1,23 @@
 import styled from "styled-components";
 
-export const DialogWrapper = styled.div`
-    position: fixed;
-    top: 0;
-    left: 0;
-    z-index: 9997;
-    width: 100%;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-`
 
 export const DialogContainer = styled.div`
     position: relative;
     background-color: #FFF;
-    padding: 5px 5px 30px 5px;
+    padding: 15px;
     box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
     z-index: 9998;
-    width: 40vw;
+    width: 500px;
     max-height: 70vh;
-    overflow: auto;
+    border-radius: 10px;
+    overflow: hidden;
+    margin: 10px;
+`
+
+export const ScrollableWrapper = styled.div`
+    width: 100%;
+    max-height: 70vh;
+    overflow-y: auto;
 `
 
 export const ButtonsWrapper = styled.div`
