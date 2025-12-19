9d25fea19313fcd2cfc58840e086e05f7eba0958
MaciejakMateusz <maciejakmateusz@gmail.com>
2025-12-19 22:24:25 +0100

    Updated TranslationStatus.jsx

--- src/app/hungryscan/cms/translations/TranslationStatus.jsx
+++ src/app/hungryscan/cms/translations/TranslationStatus.jsx
@@ -1,19 +1,26 @@
 import React from "react";
-import {AvailableIcon} from "../../../icons/AvailableIcon";
+import {CheckIcon} from "../../../icons/CheckIcon";
 import {useTranslation} from "react-i18next";
+import {CloseIcon} from "../../../icons/CloseIcon";
+import {useWindowWidth} from "../../../../hooks/useWindowWidth";
 
 export const TranslationStatus = ({translated}) => {
     const {t} = useTranslation();
+    const windowWidth = useWindowWidth();
+    const isMobile = windowWidth <= 550;
 
     return (
         <div className={`translation-status-box ${translated ? 'translated' : ''}`}>
             {translated ?
-                <span className={'translation-status-icon'}>
-                    <AvailableIcon width={'19'} height={'19'} fill={'#FFF'}/>
-                </span> : <></>}
-            <span className={`translation-status-text ${translated ? 'translated' : ''}`}>
-                {translated ? t('translation') : t('noTranslations')}
-            </span>
+                <CheckIcon width={'15'} height={'15'} stroke={'#006724'}/>
+                : <CloseIcon width={'15'} height={'15'} fill={'grey'}/>
+            }
+            {!isMobile &&
+                <span className={`translation-status-text ${translated ? 'translated' : ''}`}>
+                    {translated ? t('translation') : t('noTranslations')}
+                </span>
+            }
+
         </div>
     );
 }
