96de67a5d69ea0d7f8790b0bdf020b126032b79a
MaciejakMateusz <maciejakmateusz@gmail.com>
2026-01-09 16:34:39 +0100

    Updated Widgets.jsx

--- src/app/hungryscan/dashboard/stats/widgets/Widgets.jsx
+++ src/app/hungryscan/dashboard/stats/widgets/Widgets.jsx
@@ -4,13 +4,21 @@ import {UsersActivityWidget} from "./users-activity/UsersActivityWidget";
 import {QrFrequencyWidget} from "./qr-freq/QrFrequencyWidget";
 import {PopularDishesWidget} from "./popular-dishes/PopularDishesWidget";
 import {useWindowWidth} from "../../../../../hooks/useWindowWidth";
+import {useSelector} from "react-redux";
 
 export const Widgets = () => {
+    const {navPanelCollapsed} = useSelector(state => state.globalParams.globalParams);
     const width = useWindowWidth();
-    const isWide = width > 1300;
+    const isWide = navPanelCollapsed ? width > 1300 : width > 1450;
+
+    const columnLayout = {
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '24px'
+    }
 
     return (
-        <div className={'statistics-widgets-layout'}>
+        <div className={'statistics-widgets-layout'} style={!isWide ? columnLayout : {}}>
             <div className={'widgets-left-col'}>
                 <QrWidget/>
                 {isWide ? <UsersActivityWidget/> : <QrFrequencyWidget/>}
