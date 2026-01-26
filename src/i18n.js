e500933a2f93d7b64e32f4311f1c0bfe50f70b6a
MaciejakMateusz <maciejakmateusz@gmail.com>
2026-01-26 12:41:52 +0100

    Updated i18n.js

--- src/i18n.js
+++ src/i18n.js
@@ -2,16 +2,16 @@ import i18n from 'i18next';
 import {initReactI18next} from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-import translation_en from './locales/en/translation_en.json';
 import translation_pl from './locales/pl/translation_pl.json';
+import translation_en from './locales/en/translation_en.json';
 import translation_de from './locales/de/translation_de.json';
 import translation_fr from './locales/fr/translation_fr.json';
 import translation_es from './locales/es/translation_es.json';
 import translation_uk from './locales/uk/translation_uk.json';
 
 export const supportedLanguages = {
-    en: {translation: translation_en},
     pl: {translation: translation_pl},
+    en: {translation: translation_en},
     de: {translation: translation_de},
     fr: {translation: translation_fr},
     es: {translation: translation_es},
