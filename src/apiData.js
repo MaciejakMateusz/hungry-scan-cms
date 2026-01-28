04df673172708c132e540e4ff9ca44cbebadbc4f
MaciejakMateusz <maciejakmateusz@gmail.com>
2026-01-28 19:06:05 +0100

    Updated apiData.js

--- src/apiData.js
+++ src/apiData.js
@@ -1,3 +1,4 @@
 export const apiHost = process.env.REACT_APP_CORE_HOST;
 export const menuAppHost = process.env.REACT_APP_MENU_HOST;
 export const s3BucketUrl = process.env.REACT_APP_S3_BUCKET;
+export const fallbackLng = process.env.REACT_APP_FALLBACK_LANGUAGE;
