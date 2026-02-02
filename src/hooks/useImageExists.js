fde7c5649fe5b3d5bfa729bd76801210bd60d99c
MaciejakMateusz <maciejakmateusz@gmail.com>
2026-02-02 12:31:55 +0100

    Updated useImageExists.js

--- src/hooks/useImageExists.js
+++ src/hooks/useImageExists.js
@@ -1,13 +1,15 @@
 import {useEffect, useState} from "react";
 import {s3BucketUrl} from "../apiData";
 
-export const useImageExists = id => {
+export const useImageExists = obj => {
     const [exists, setExists] = useState(null);
+    const id = obj?.id ?? 0;
+    const updatedAt = obj?.updated;
 
     useEffect(() => {
         setExists(null);
         let cancelled = false;
-        const url = `${s3BucketUrl}/menuItems/${id}.png`;
+        const url = `${s3BucketUrl}/menuItems/${id}.png?cb=${updatedAt}`;
         const img = new Image();
         img.onload = () => {
             if (!cancelled) setExists(img.naturalWidth > 0);
@@ -21,7 +23,7 @@ export const useImageExists = id => {
             cancelled = true;
             img.src = "";
         };
-    }, [id]);
+    }, [id, updatedAt]);
 
     return exists;
 }
