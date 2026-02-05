cab0f520f27e82a35a5e9055739e081fe8fc19a2
MaciejakMateusz <maciejakmateusz@gmail.com>
2026-02-05 15:12:53 +0100

    Updated Tooltip.jsx

--- src/app/hungryscan/cms/dishes-categories/menu-item/preview/menu-item-details/Tooltip.jsx
+++ src/app/hungryscan/cms/dishes-categories/menu-item/preview/menu-item-details/Tooltip.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Tippy from "@tippyjs/react";
 
-export const Tooltip = ({children, content, appendTo}) => {
+export const Tooltip = ({children, content, appendTo = document.body, className = 'tooltip'}) => {
     return (
         <Tippy content={content}
                placement={"auto"}
                delay={0}
                duration={0}
-               className={'tooltip'}
+               className={className}
                appendTo={appendTo}>
             {children}
         </Tippy>
