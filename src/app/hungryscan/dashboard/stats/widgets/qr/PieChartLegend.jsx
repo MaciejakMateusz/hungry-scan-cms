74072c5637089da8dda6fee698428e8c5b6e2f2f
MaciejakMateusz <maciejakmateusz@gmail.com>
2026-02-05 15:12:40 +0100

    Updated PieChartLegend.jsx

--- src/app/hungryscan/dashboard/stats/widgets/qr/PieChartLegend.jsx
+++ src/app/hungryscan/dashboard/stats/widgets/qr/PieChartLegend.jsx
@@ -14,8 +14,11 @@ import {
     UniqueScanValue,
     UniqueValuesWrapper
 } from "./PieChartLegend.style";
+import {Tooltip} from "../../../../cms/dishes-categories/menu-item/preview/menu-item-details/Tooltip";
+import {useTranslation} from "react-i18next";
 
 export const PieChartLegend = ({chartData}) => {
+    const {t} = useTranslation();
 
     const renderUniqueScanDifference = periodDifference => {
         const isPositive = periodDifference >= 0;
@@ -39,17 +42,29 @@ export const PieChartLegend = ({chartData}) => {
         <foreignObject x={200} y={85} width={200} height={50}>
             <div xmlns="http://www.w3.org/1999/xhtml">
                 <LegendContainer>
-                    <UniqueScanIconContainer><UniqueScanIcon/></UniqueScanIconContainer>
-                    <RepeatedScanIconContainer><RepeatedScanIcon/></RepeatedScanIconContainer>
-                    <UniqueScanLabel>{chartData[0].label}</UniqueScanLabel>
-                    <RepeatedScanLabel>{chartData[1].label}</RepeatedScanLabel>
+                    <UniqueScanIconContainer>
+                        <UniqueScanIcon/>
+                    </UniqueScanIconContainer>
+                    <RepeatedScanIconContainer>
+                        <RepeatedScanIcon/>
+                    </RepeatedScanIconContainer>
+                    <UniqueScanLabel>
+                        <span>{chartData[0].label}</span>
+                    </UniqueScanLabel>
+                    <RepeatedScanLabel>
+                        <span>{chartData[1].label}</span>
+                    </RepeatedScanLabel>
                     <UniqueValuesWrapper>
                         <UniqueScanValue>{chartData[0].value}</UniqueScanValue>
-                        {renderUniqueScanDifference(chartData[0].periodDifference)}
+                        <Tooltip content={t('uniqueScansMetricInfo')}>
+                            {renderUniqueScanDifference(chartData[0].periodDifference)}
+                        </Tooltip>
                     </UniqueValuesWrapper>
                     <RepeatedValuesWrapper>
                         <RepeatedScanValue>{chartData[1].value}</RepeatedScanValue>
-                        {renderRepeatedScanDifference(chartData[1].periodDifference)}
+                        <Tooltip content={t('repeatedScansMetricInfo')}>
+                            {renderRepeatedScanDifference(chartData[1].periodDifference)}
+                        </Tooltip>
                     </RepeatedValuesWrapper>
                 </LegendContainer>
             </div>
